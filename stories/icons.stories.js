541eba9424d58943b17d76dbe617944bcfc1de5c
Adnane <adnane.tellou@symphony.com>
2020-05-11 17:02:01 +0200

    APP-2672 - Initialize UI Toolkit Styles lib

---
+++ stories/icons.stories.js
@@ -0,0 +1,7 @@
+export default {
+  title: 'Icons',
+};
+
+export const Icon = () => `
+<i class="tk-ic-lock"></i>
+<i class="tk-ic-reload animate-spin"></i>`;
---
+++ stories/text.stories.js
@@ -0,0 +1,7 @@
+export default {
+  title: 'Text',
+};
+
+export const InputLabel = () => `<label class="tk-label">Some label</label><br><br>
+<label class="tk-label">Some label</label>
+<input class="tk-input" value="Some value">`;
