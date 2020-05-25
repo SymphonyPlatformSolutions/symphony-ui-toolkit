b49ba7763d4cd2ac6780e368d1d0f9182df6a282
Adnane Tellou <63653674+symphony-adnane@users.noreply.github.com>
2020-05-25 16:07:02 +0200

    Change size to Em + Disable snapshot publish (#6)
    * APP-2672 - Initialize UI Toolkit Styles lib
    
    * Switch button to use em + remove auto snapshot publish

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,18 @@
 import '!style-loader!css-loader!sass-loader!../stories/stories.scss';
-
+import { addParameters } from '@storybook/html';
 import addons from '@storybook/addons';
+import { themes } from '@storybook/theming';
 
-setTimeout(() => init(), 1);
-
+setTimeout(() => init());
+addParameters({
+  darkMode: {
+    // Override the default dark theme
+    dark: { ...themes.dark, appContentBg: '#1A1C1D' },
+  },
+});
 // this allows us to add a dark class to body element to be able to render our component in dark mode  in sync with storybook's
 const init = () => {
-  const darkClass = 'tk-dark';
+  const darkClass = 'dark';
   const channel = addons.getChannel();
   channel.on('DARK_MODE', (isDark) => {
     if (isDark) {
