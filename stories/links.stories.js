bddf756fda75734a89267e0206e8821c8a9dd83a
Mathieu Declercq <mathieu.declercq@symphony.com>
2020-06-01 17:18:35 +0200

    APP-2718 - Button links

--- stories/links.stories.js
+++ stories/links.stories.js
@@ -4,7 +4,5 @@ export default {
 
 export const Links = () => `
   <a class="tk-link" href="https://www.symphony.com" target="_blank">Text link</a><br><br>
-  <a class="tk-link" href="https://www.symphony.com" target="_blank">
-    <button class="tk-button tk-button--primary">Button Link</button>
-  </a>
+  <button class="tk-link">Button Link</button>
 `;
