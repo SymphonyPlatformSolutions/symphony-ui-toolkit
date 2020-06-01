cff9fdc923aeec1403d93022963f843080eb782c
Mathieu Declercq <mathieu.declercq@symphony.com>
2020-06-01 16:30:10 +0200

    APP-2718 - Create tk-link class

---
+++ stories/links.stories.js
@@ -0,0 +1,10 @@
+export default {
+  title: 'Link',
+};
+
+export const Links = () => `
+  <a class="tk-link" href="https://www.symphony.com" target="_blank">Text link</a><br><br>
+  <a class="tk-link" href="https://www.symphony.com" target="_blank">
+    <button class="tk-button tk-button--primary">Button Link</button>
+  </a>
+`;
