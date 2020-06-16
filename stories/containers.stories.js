8c478900eec67583ff3c588df858068d1ec05fa3
Adnane Tellou <63653674+symphony-adnane@users.noreply.github.com>
2020-06-16 09:38:25 +0200

    APP-2719 - Add quote container (#13)

---
+++ stories/containers.stories.js
@@ -0,0 +1,18 @@
+export default {
+  title: 'Containers',
+};
+
+export const Quote = () => `
+  <div class="flex-row">
+  <div class="flex-col">
+      <div class="tk-quote-container tk-quote-container--default">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+      </div>
+  </div>
+  <div class="flex-col">
+      <div class="tk-quote-container tk-quote-container--error">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+      </div>
+  </div>
+</div>
+  `;
