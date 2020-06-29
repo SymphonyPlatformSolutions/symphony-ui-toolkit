cfd5da29d78ddb33dfa5cfbb552f39d597e18bb9
Adnane <adnane.tellou@symphony.com>
2020-06-29 16:17:09 +0200

    Init expandable card component

---
+++ src/components/expandable-card/ExpandableCard.tsx
@@ -0,0 +1,82 @@
+import PropTypes from 'prop-types';
+import * as React from 'react';
+import styled from 'styled-components';
+
+type ExpandableCardProps = {
+  children?: React.ReactNode;
+  header: React.ReactNode;
+  className?: string;
+  initCollapsed?: boolean;
+};
+// styled components, if it grows put in a sibling file
+const HeaderDiv = styled.div`
+  & > div {
+    display: inline-block;
+    margin-right: 8px;
+    margin-bottom: 8px;
+  }
+  & > .tk-link.toggle {
+    margin-left: 8px;
+    text-decoration: none;
+    display: inline-block;
+  }
+  & > .tk-ic-top {
+    &::before {
+      transition: transform 300ms ease-in-out;
+    }
+    &.collapsed::before {
+      transform: rotate(180deg);
+    }
+    cursor: pointer;
+  }
+`;
+
+const BodyDiv = styled.div`
+  max-height: 100vh;
+  overflow: hidden;
+  transition: max-height 300ms ease-in-out;
+  &.collapsed {
+    max-height: 0;
+  }
+`;
+
+export default class ExpandableCard extends React.Component<
+  ExpandableCardProps
+> {
+  public state = {
+    collapsed:
+      this.props.initCollapsed === undefined || this.props.initCollapsed
+  };
+
+  onToggle() {
+    this.setState({ collapsed: !this.state.collapsed });
+  }
+
+  render() {
+    const collapsedClass = this.state.collapsed ? ' collapsed' : '';
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { initCollapsed, header, children, ...rest } = this.props;
+    return (
+      <div {...rest}>
+        <HeaderDiv>
+          <div>{this.props.header}</div>
+          &bull;
+          <a className="tk-link toggle" onClick={this.onToggle.bind(this)}>
+            {this.state.collapsed ? 'EXPAND' : 'COLLAPSE'}
+          </a>
+          <i
+            className={'tk-ic-top' + collapsedClass}
+            onClick={this.onToggle.bind(this)}
+            aria-label="Toggle"
+          ></i>
+        </HeaderDiv>
+        <BodyDiv className={collapsedClass}>{children}</BodyDiv>
+      </div>
+    );
+  }
+
+  static propTypes = {
+    initCollapsed: PropTypes.bool,
+    header: PropTypes.node.isRequired
+  };
+}
---
+++ stories/ExpandableCard.stories.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { ExpandableCard, Button, CropContent } from '../src/components';
+
+export const ExpandableCardContainer: React.SFC = () => {
+  return (
+    <>
+      <div className="flex-row">
+        <div className="flex-col" style={{ maxWidth: '50%' }}>
+          <h2>Basic card</h2>
+          <ExpandableCard
+            header={
+              <div>
+                An example of a header with a <a className="tk-link">link</a>
+              </div>
+            }
+          >
+            <CropContent className="tk-quote-container tk-quote-container--error">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+            </CropContent>
+          </ExpandableCard>
+        </div>
+        <div className="flex-col">
+          <h2>Initially expanded card</h2>
+          <ExpandableCard
+            header={<Button>An example of custom header</Button>}
+            initCollapsed={false}
+          >
+            <CropContent className="tk-quote-container">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+            </CropContent>
+          </ExpandableCard>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default {
+  title: 'Containers'
+};
