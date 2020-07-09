767bf4cd7030afba0dacec9b886c86de015d8cd6
Jean-Michael Legait <jean-michael.legait@symphony.com>
2020-07-09 20:46:44 +0200

    PLAT-9198 Use react-popper

--- src/components/info-hint/InfoHint.tsx
+++ src/components/info-hint/InfoHint.tsx
@@ -2,12 +2,33 @@ import PropTypes from 'prop-types';
 import React, { useState, useMemo } from 'react';
 import InfoHintIcon from './InfoHintIcon';
 import shortid from 'shortid';
-import Tooltip from '../tooltip';
+import { usePopper } from 'react-popper';
 
 const InfoHint = ({ title }) => {
   const ariaId = useMemo(() => `hint-${shortid.generate()}`, []);
 
   const [tooltipShown, setInfoHintShown] = useState(false);
+
+  const [referenceElement, setReferenceElement] = useState(null);
+  const [popperElement, setPopperElement] = useState(null);
+  const [arrowElement, setArrowElement] = useState(null);
+  const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    placement: 'top',
+    modifiers: [
+      {
+        name: 'flip',
+        options: {
+          fallbackPlacements: ['bottom', 'right', 'left'],
+        },
+      },
+      {
+        name: 'offset',
+        options: {
+          offset: [0, 15],
+        },
+      },
+    ],
+  });
 
   const handleClick = () => {
     setInfoHintShown(!tooltipShown);
@@ -18,9 +39,28 @@ const InfoHint = ({ title }) => {
   };
 
   return (
-    <span onClick={handleClick} className="info-hint" title={title}>
-      <InfoHintIcon className="info-hint__icon" />
-    </span>
+    <>
+      <span
+        onClick={handleClick}
+        className="info-hint"
+        ref={setReferenceElement}
+        title={title}
+        aria-describedby={ariaId}
+      >
+        <InfoHintIcon className="info-hint__icon" />
+      </span>
+
+      {tooltipShown && (
+        <div
+          ref={setPopperElement}
+          style={styles.popper}
+          {...attributes.popper}
+        >
+          Popper element
+          <div ref={setArrowElement} style={styles.arrow} />
+        </div>
+      )}
+    </>
   );
 };
 
