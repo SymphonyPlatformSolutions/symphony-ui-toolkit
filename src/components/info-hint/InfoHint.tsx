d2be5698aa8dc9c90690641a01908d7dbbea196c
Jean-Michael Legait <jean-michael.legait@symphony.com>
2020-07-10 15:01:44 +0200

    PLAT-9198 Move tooltip feature in a Tooltip component

--- src/components/info-hint/InfoHint.tsx
+++ src/components/info-hint/InfoHint.tsx
@@ -1,11 +1,8 @@
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import InfoHintIcon from './InfoHintIcon';
-import { usePopper } from 'react-popper';
-import { CSSTransition } from 'react-transition-group';
 import styled from 'styled-components';
-
-import './style.css';
+import Tooltip from '../tooltip';
 
 const InfoHintSpan = styled.span`
   display: inline-block;
@@ -13,87 +10,10 @@ const InfoHintSpan = styled.span`
   height: 16px;
 `;
 
-const TooltipContainer = styled.div`
-  border-radius: 8px;
-  background-color: #333436;
-  padding: 16px;
-  text-align: center;
-  color: #ffffff;
-  // font-family: Segoe UI;
-  font-size: 14px;
-  line-height: 20px;
-  white-space: pre-line;
-  min-width: 200px;
-  max-width: 500px;
-
-  .arrow {
-    position: absolute;
-    width: 24px;
-    height: 24px;
-    z-index: -1;
-    border-radius: 2px;
-    &::before {
-      position: absolute;
-      left: 0;
-      width: 24px;
-      height: 24px;
-      z-index: -1;
-      border-radius: 2px;
-      content: '';
-      transform: rotate(45deg);
-      background: #333436;
-    }
-  }
-
-  &[data-popper-placement^='top'] > .arrow {
-    bottom: -7px;
-  }
-  &[data-popper-placement^='bottom'] > .arrow {
-    top: -7px;
-  }
-  &[data-popper-placement^='left'] > .arrow {
-    right: -7px;
-  }
-  &[data-popper-placement^='right'] > .arrow {
-    left: -7px;
-  }
-`;
-
-const TooltipFooter = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  margin-top: 8px;
-`;
-
-const TooltipClose = styled.span`
-  text-transform: uppercase;
-  cursor: pointer;
-  padding: 8px 16px;
-`;
-
 const InfoHint = ({ id, title }) => {
   const [tooltipShown, setInfoHintShown] = useState(false);
 
   const [referenceElement, setReferenceElement] = useState(null);
-  const [popperElement, setPopperElement] = useState(null);
-  const [arrowElement, setArrowElement] = useState(null);
-  const { styles, attributes } = usePopper(referenceElement, popperElement, {
-    placement: 'top',
-    modifiers: [
-      {
-        name: 'flip',
-        options: {
-          fallbackPlacements: ['bottom', 'right', 'left'],
-        },
-      },
-      {
-        name: 'offset',
-        options: {
-          offset: [0, 15],
-        },
-      },
-    ],
-  });
 
   const handleClick = () => {
     setInfoHintShown(!tooltipShown);
@@ -114,37 +34,13 @@ const InfoHint = ({ id, title }) => {
         <InfoHintIcon className="info-hint__icon" />
       </InfoHintSpan>
 
-      <CSSTransition
-        unmountOnExit
-        in={tooltipShown}
-        timeout={200}
-        classNames="TooltipContainer"
-        appear
-      >
-        <TooltipContainer
-          id={id}
-          role="tooltip"
-          ref={setPopperElement}
-          style={styles.popper}
-          {...attributes.popper}
-        >
-          {title}
-          <div
-            ref={setArrowElement}
-            style={styles.arrow}
-            className="arrow"
-            data-popper-arrow
-          />
-          <TooltipFooter className="cta-container">
-            <TooltipClose
-              className="cta-container__text"
-              onClick={handleHintClose}
-            >
-              Got it
-            </TooltipClose>
-          </TooltipFooter>
-        </TooltipContainer>
-      </CSSTransition>
+      <Tooltip
+        id={id}
+        onHintClose={handleClick}
+        description={title}
+        visible={tooltipShown}
+        referenceElement={referenceElement}
+      />
     </>
   );
 };
--- src/components/tooltip/Tooltip.tsx
+++ src/components/tooltip/Tooltip.tsx
@@ -1,9 +1,85 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { usePopper } from 'react-popper';
+import { CSSTransition } from 'react-transition-group';
+import styled from 'styled-components';
 
-const Tooltip = ({ id, visible, onHintClose, dataTitle, children }) => {
-  const [referenceElement, setReferenceElement] = useState(null);
+import './style.css';
+
+const TooltipContainer = styled.div`
+  border-radius: 8px;
+  background-color: #333436;
+  padding: 16px;
+  text-align: center;
+  color: #ffffff;
+  // font-family: Segoe UI;
+  font-size: 14px;
+  line-height: 20px;
+  white-space: pre-line;
+  min-width: 200px;
+  max-width: 500px;
+
+  .arrow {
+    position: absolute;
+    width: 24px;
+    height: 24px;
+    z-index: -1;
+    border-radius: 2px;
+    &::before {
+      position: absolute;
+      left: 0;
+      width: 24px;
+      height: 24px;
+      z-index: -1;
+      border-radius: 2px;
+      content: '';
+      transform: rotate(45deg);
+      background: #333436;
+    }
+  }
+
+  &[data-popper-placement^='top'] > .arrow {
+    bottom: -7px;
+  }
+  &[data-popper-placement^='bottom'] > .arrow {
+    top: -7px;
+  }
+  &[data-popper-placement^='left'] > .arrow {
+    right: -7px;
+  }
+  &[data-popper-placement^='right'] > .arrow {
+    left: -7px;
+  }
+`;
+
+const TooltipFooter = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 8px;
+`;
+
+const TooltipClose = styled.span`
+  text-transform: uppercase;
+  cursor: pointer;
+  padding: 8px 16px;
+`;
+
+/**
+ * Tooltip component
+ * @param id CSS ID
+ * @param description Text to display in the tooltip
+ * @param visible true if the tooltip should be displayed, false otherwise
+ * @param onHintClose Function to call on close action
+ * @param referenceElement HTML Element on which the Tooltip must be positioned
+ * @constructor
+ */
+const Tooltip = ({
+  id,
+  description,
+  visible,
+  onHintClose,
+  referenceElement,
+}) => {
   const [popperElement, setPopperElement] = useState(null);
   const [arrowElement, setArrowElement] = useState(null);
   const { styles, attributes } = usePopper(referenceElement, popperElement, {
@@ -22,124 +98,46 @@ const Tooltip = ({ id, visible, onHintClose, dataTitle, children }) => {
         },
       },
     ],
-
-    //modifiers: [{ name: 'arrow', options: { element: arrowElement } }],
   });
 
-  // const mountInfoHintTitle = () => {
-  //   // If a hinfoHintTitle was going to be unmounted, we unmount it now.
-  //   if (ongoingTimeout) {
-  //     this.flushUngoingTimeout();
-  //   }
-  //   this.hinfoHintTitleInstance = createPopper(
-  //     this.target,
-  //     this.hinfoHintTitleContent,
-  //     {
-  //       placement: 'top',
-  //       scroll: false,
-  //       resize: false,
-  //       modifiers: [
-  //         {
-  //           name: 'flip',
-  //           options: {
-  //             fallbackPlacements: ['bottom', 'right', 'left'],
-  //           },
-  //         },
-  //         {
-  //           name: 'offset',
-  //           options: {
-  //             offset: [0, 15],
-  //           },
-  //         },
-  //       ],
-  //     }
-  //   );
-  // };
-  //
-  // const flushUngoingTimeout = () => {
-  //   clearTimeout(ongoingTimeout);
-  //   Reflect.deleteProperty(this, 'ongoingTimeout');
-  //   unmountInfoHintTitle();
-  // };
-
-  // const renderTooltip = (id, visible, onHintClose, dataTitle) => {
-  //   // const { translation } = {};//this.state;
-  //
-  //   return (
-  //     <div
-  //       // ref={(el) => (this.hinfoHintTitleContent = el)}
-  //       data-show={visible}
-  //       className="info-hint__title"
-  //       id={id}
-  //     >
-  //       {dataTitle}
-  //       <div
-  //         /*ref={(el) => (this.arrowEl = el)}*/ className="arrow"
-  //         data-popper-arrow
-  //       />
-  //       <div className="cta-container">
-  //         <div
-  //           className="cta-container__text"
-  //           onClick={(e) => onHintClose(e)}
-  //         ></div>
-  //       </div>
-  //     </div>
-  //   );
-  // };
-
   return (
-    <>
-      {children && children({ targetRef: referenceElement })}
-      {/*<button type="button" ref={setReferenceElement}>*/}
-      {/*  Reference element*/}
-      {/*</button>*/}
-
-      <div
-        ref={popperElement}
-        data-show={visible}
-        className="info-hint__title"
+    <CSSTransition
+      unmountOnExit
+      in={visible}
+      timeout={200}
+      classNames="TooltipContainer"
+      appear
+    >
+      <TooltipContainer
         id={id}
+        role="tooltip"
+        ref={setPopperElement}
+        style={styles.popper}
+        {...attributes.popper}
       >
-        {dataTitle}
-        <div ref={setArrowElement} className="arrow" data-popper-arrow />
-        <div className="cta-container">
-          <div
-            className="cta-container__text"
-            onClick={(e) => onHintClose(e)}
-          ></div>
-        </div>
-      </div>
-    </>
+        {description}
+        <div
+          ref={setArrowElement}
+          style={styles.arrow}
+          className="arrow"
+          data-popper-arrow
+        />
+        <TooltipFooter className="cta-container">
+          <TooltipClose className="cta-container__text" onClick={onHintClose}>
+            Got it
+          </TooltipClose>
+        </TooltipFooter>
+      </TooltipContainer>
+    </CSSTransition>
   );
-
-  // return (
-  //   <>
-  //     {children && children({})}
-  //     <button type="button" ref={setReferenceElement}>
-  //       Reference element
-  //     </button>
-  //
-  //     <div ref={setPopperElement} style={styles.popper} {...attributes.popper}>
-  //       Popper element
-  //       <div ref={setArrowElement} style={styles.arrow} />
-  //     </div>
-  //   </>
-  // );
-
-  // return (
-  //   <>
-  //     {children && children({})}
-  //     {renderTooltip(id, visible, onHintClose, dataTitle)}
-  //   </>
-  // );
 };
 
 Tooltip.propTypes = {
-  id: PropTypes.string,
-  children: PropTypes.func,
-  dataTitle: PropTypes.string.isRequired,
-  onHintClose: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
   visible: PropTypes.bool.isRequired,
+  onHintClose: PropTypes.func.isRequired,
+  referenceElement: PropTypes.object,
 };
 
 export default Tooltip;
