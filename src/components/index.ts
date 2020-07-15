c17537678c8a5f95355c2e58ac6cf4d7fc8acbf7
Jean-Michael Legait <jean-michael.legait@symphony.com>
2020-07-15 11:18:47 +0200

    PLAT-9198 Remove Icon and Tooltip from public components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,6 @@
 import Button from './button';
-import Icon from './icon';
 import Input from './input';
 import CropContent from './crop-content/CropContent';
 import ExpandableCard from './expandable-card/ExpandableCard';
-import Tooltip from './tooltip';
 
-export { Button, Icon, Input, CropContent, ExpandableCard, Tooltip };
+export { Button, Input, CropContent, ExpandableCard };
