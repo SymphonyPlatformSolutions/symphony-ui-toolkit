b9e3f5791b9713fb3631c6958dd4b483af421c99
Jean-Michael Legait <jean-michael.legait@symphony.com>
2020-07-15 10:14:02 +0200

    PLAT-9198 Export Icon

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,8 @@
 import Button from './button';
+import Icon from './icon';
 import Input from './input';
 import CropContent from './crop-content/CropContent';
 import ExpandableCard from './expandable-card/ExpandableCard';
 import Tooltip from './tooltip';
 
-export { Button, Input, CropContent, ExpandableCard, Tooltip };
+export { Button, Icon, Input, CropContent, ExpandableCard, Tooltip };
