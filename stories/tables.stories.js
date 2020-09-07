843a8da03a87a0e81d57804ca70e63d90c62c954
Anna Llorens <68587873+symphony-annaLlorens@users.noreply.github.com>
2020-09-07 19:35:47 +0200

    APP-2868  UIToolkit Styles - Table (#28)
    * APP-2868: Added grid to handle margin and padding
    
    * Update PR comments
    
    * Factorize spacing
    
    * Move colors
    
    * Improve storybook example
    
    * Rename spacing function
    
    * APP-2628: Add table component style
    
    * Add tab for tables on storybook

---
+++ stories/tables.stories.js
@@ -0,0 +1,63 @@
+export default {
+  title: 'Tables',
+};
+
+export const Tables = () =>
+  `<h1>Tables</h1>
+  <h2 class="tk-mt-4h">Spacing default</h2>
+  <table class="tk-table">
+  <thead>
+    <th>Name</th>
+    <th>First</th>
+    <th>Last</th>
+    <th>Country</th>
+  </thead>
+  <tr>
+    <td>Suggested</td>
+    <td>David</td>
+    <td>Smith</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Suggested</td>
+    <td>David</td>
+    <td>Smith</td>
+    <td>Belgium</td>
+  </tr>
+  <tr>
+    <td>Suggested</td>
+    <td>David</td>
+    <td>Smith</td>
+    <td>France</td>
+  </tr>
+</table>
+  <h2 class="tk-mt-4h">Spacing condensed</h2>
+  <div class="tk-theme-condensed">
+    <table class="tk-table">
+      <thead>
+        <th>Name</th>
+        <th>First</th>
+        <th>Last</th>
+        <th>Country</th>
+      </thead>
+      <tr>
+        <td>Suggested</td>
+        <td>David</td>
+        <td>Smith</td>
+        <td>Germany</td>
+      </tr>
+      <tr>
+        <td>Suggested</td>
+        <td>David</td>
+        <td>Smith</td>
+        <td>Belgium</td>
+      </tr>
+      <tr>
+        <td>Suggested</td>
+        <td>David</td>
+        <td>Smith</td>
+        <td>France</td>
+      </tr>
+    </table>
+  </div>
+`;
