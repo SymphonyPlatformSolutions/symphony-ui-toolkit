3dc82e9cb4b914e7493d95ad52d27c59adec5edb
Jean-Michael Legait <jean-michael.legait@symphony.com>
2020-09-18 18:44:03 +0200

    PLAT-9291 Cursor pointer only if a click action is defined

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -1,22 +1,16 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import styled from 'styled-components';
 import InfoRoundIcon from './InfoRoundIcon';
 
-const IconTag = styled.i`
-  cursor: pointer;
-`;
-
-const Icon = ({ iconName, handleClick }) => {
-  return (
-    <IconTag
-      className={`tk-ic-${iconName != 'info-round' ? iconName : ''} tk-icon`}
-      onClick={handleClick}
-    >
-      {iconName == 'info-round' ? <InfoRoundIcon /> : null}
-    </IconTag>
-  );
-};
+const Icon = ({ iconName, handleClick }) => (
+  <i
+    style={{ cursor: handleClick && 'pointer' }}
+    className={`tk-ic-${iconName != 'info-round' ? iconName : ''} tk-icon`}
+    onClick={handleClick}
+  >
+    {iconName == 'info-round' ? <InfoRoundIcon /> : null}
+  </i>
+);
 
 Icon.propTypes = {
   iconName: PropTypes.string.isRequired,
