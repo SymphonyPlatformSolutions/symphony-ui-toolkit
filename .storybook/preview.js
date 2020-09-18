0f9892f6a2aece05946efd37e4514c99a7b4d608
Jean-Michael Legait <jean-michael.legait@symphony.com>
2020-09-18 16:32:53 +0200

    PLAT-9291 Upgrade Storybook to v6

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -8,15 +8,15 @@ setTimeout(() => init());
 addParameters({
   darkMode: {
     // Override the default dark theme
-    dark: { ...themes.dark, appContentBg: '#1A1C1D' }
-  }
+    dark: { ...themes.dark, appContentBg: '#1A1C1D' },
+  },
 });
 
 // this allows us to add a dark class to body element to be able to render our component in dark mode  in sync with storybook's
 const init = () => {
   const darkClass = 'dark';
   const channel = addons.getChannel();
-  channel.on('DARK_MODE', isDark => {
+  channel.on('DARK_MODE', (isDark) => {
     if (isDark) {
       document.body.classList.add(darkClass);
     } else {
