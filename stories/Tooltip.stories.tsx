1df5c4c65e474231be8adb441e8c51b077aa6ee5
Jean-Michael Legait <jean-michael.legait@symphony.com>
2020-09-18 18:44:22 +0200

    PLAT-9291 Stories for Tooltip

--- stories/Tooltip.stories.tsx
+++ stories/Tooltip.stories.tsx
@@ -38,15 +38,47 @@ export const Standard = Template.bind({});
 
 Standard.args = {
   description: 'Some text',
+  placement: 'top',
   children: <input type="text" name="fname" />,
 };
 
 export const WithACloseAction = Template.bind({});
 WithACloseAction.args = {
   description: 'Some text',
-  placement: 'right',
+  placement: 'top',
   closeLabel: 'Close',
   children: <input type="text" name="fname" />,
+};
+
+export const ShowHide = Template.bind({});
+ShowHide.args = {
+  description: 'Toggle the visibility of the Tooltip in the Controls tab',
+  placement: 'top',
+  visible: true,
+  children: <input type="text" name="fname" />,
+};
+ShowHide.argTypes = {
+  placement: { control: { disable: true } },
+};
+
+export const OnClick = (args) => {
+  const [showTooltip, setShowTooltip] = useState(true);
+
+  const handleClick = () => {
+    setShowTooltip(!showTooltip);
+  };
+
+  return (
+    <Tooltip {...args} visible={showTooltip} onHintClose={handleClick}>
+      <Icon iconName="info-round" handleClick={handleClick} />
+    </Tooltip>
+  );
+};
+OnClick.args = {
+  description: 'Click on the icon',
+  closeLabel: 'Close',
+  placement: 'top',
+  children: <button type={'button'}>A button</button>,
 };
 
 export const Placements = Template.bind({});
@@ -56,114 +88,46 @@ Placements.args = {
   children: <input type="text" name="fname" />,
 };
 
-// export const WithACloseAction = Template.bind({});
-// WithACloseAction.args = {
-//   description: 'Some text',
-//   placement: 'right',
-//   closeLabel: 'Close',
-//   children: <Icon iconName="info-round" handleClick={handleClickIcon} />,
-// };
+export const OnAButton = Template.bind({});
+OnAButton.args = {
+  description: 'Attached on a button',
+  placement: 'top',
+  children: <button type={'button'}>A button</button>,
+};
 
-// export const WithACloseAction = Template.bind({});
-// WithACloseAction.args = {
-//   description: 'Some text',
-//   placement: 'right',
-//   closeLabel: 'Close',
-//   children: <Icon iconName="info-round" handleClick={handleClickIcon} />,
-// };
+export const OnATextField = Template.bind({});
+OnATextField.args = {
+  description: 'Attached on a text field',
+  placement: 'top',
+  children: <input type="text" name="textTextField" />,
+};
 
-// export const Tooltips = () => {
-//   return (
-//     <div className="tk-text-color">
-//       <h1>Tooltip</h1>
-//       <h2>Standard</h2>
-//       <div style={{ margin: '100px 150px 0 50px' }}>
-//         <Tooltip description={'Some text'} visible={true} placement={'top'}>
-//           <input type="text" name="fname" />
-//         </Tooltip>
-//       </div>
-//       <h2>With a close action</h2>
-//       <div style={{ margin: '50px 150px 50px 50px' }}>
-//         <Tooltip
-//           description={'Some text'}
-//           visible={showTooltip}
-//           placement={'right'}
-//           closeLabel={'Close'}
-//           onHintClose={handleClick}
-//         >
-//           <input type="text" name="fname" />
-//         </Tooltip>
-//       </div>
-//       <h2>With a show/hide action</h2>
-//       <p>Click on the icon</p>
-//       <div style={{ margin: '120px 0 50px 120px' }}>
-//         <Tooltip
-//           description={'Some text'}
-//           visible={showTooltipIcon}
-//           closeLabel={'Close'}
-//           onHintClose={handleClickIcon}
-//         >
-//           <Icon iconName="info-round" handleClick={handleClickIcon} />
-//         </Tooltip>
-//       </div>
-//       <h2>Can be attached to anything</h2>
-//       <p>You can find below some examples</p>
-//       <div style={{ margin: '100px 150px 0 100px' }}>
-//         <Tooltip description={'Attached to a button'} visible={true}>
-//           <button className={'test'} type={'button'}>
-//             A button
-//           </button>
-//         </Tooltip>
-//       </div>
-//       <div style={{ margin: '100px 0 0 50px' }}>
-//         <Tooltip description={'Attached to a text field'} visible={true}>
-//           <input type="text" name="textTextField" />
-//         </Tooltip>
-//       </div>
-//       <div style={{ margin: '100px 150px 0 0' }}>
-//         <Tooltip description={'Attached to a text area'} visible={true}>
-//           <textarea id="w3review" name="w3review" rows="4" cols="50">
-//             Some text
-//           </textarea>
-//         </Tooltip>
-//       </div>
-//       <div style={{ margin: '100px 150px 0 100px' }}>
-//         <Tooltip description={'Attached to a date picker'} visible={true}>
-//           <input type="date" id="birthday" name="birthday" />
-//         </Tooltip>
-//       </div>
-//       <div style={{ margin: '100px 150px 0 100px' }}>
-//         <Tooltip description={'Attached to an image'} visible={true}>
-//           <img src="https://symphony.com/wp-content/uploads/2019/06/logo.png" />
-//         </Tooltip>
-//       </div>
-//       <h2>Tooltip Placements</h2>
-//       <div style={{ margin: '50px 150px 0 150px' }}>
-//         <Tooltip description={'A text'} visible={true} placement={'top'}>
-//           <button className={'test'} type={'button'}>
-//             A button
-//           </button>
-//         </Tooltip>
-//       </div>
-//       <div style={{ margin: '50px' }}>
-//         <Tooltip description={'Some text'} visible={true} placement={'right'}>
-//           <button className={'test'} type={'button'}>
-//             A button
-//           </button>
-//         </Tooltip>
-//       </div>
-//       <div style={{ margin: '80px 50px 50px 250px' }}>
-//         <Tooltip description={'Some text'} visible={true} placement={'left'}>
-//           <button style={{ marginRight: '0' }} type={'button'}>
-//             A button
-//           </button>
-//         </Tooltip>
-//       </div>
-//       <div style={{ margin: '0 200px 100px' }}>
-//         <Tooltip description={'Some text'} visible={true} placement={'bottom'}>
-//           <button type={'button'}>A button</button>
-//         </Tooltip>
-//       </div>
-//     </div>
-//   );
-// };
+export const OnATextArea = Template.bind({});
+OnATextArea.args = {
+  description: 'Attached on a text area',
+  placement: 'top',
+  children: <textarea name="aTextArea" rows="4" cols="50" value="Some text" />,
+};
+
+export const OnAnIcon = Template.bind({});
+OnAnIcon.args = {
+  description: 'Attached on an icon',
+  placement: 'top',
+  children: <Icon iconName="info-round" />,
+};
+
+export const OnADatePicker = Template.bind({});
+OnADatePicker.args = {
+  description: 'Attached on a date picker',
+  placement: 'top',
+  children: <input type="date" id="meeting" name="meeting" />,
+};
+
+export const OnAnImage = Template.bind({});
+OnAnImage.args = {
+  description: 'Attached on an image',
+  placement: 'top',
+  children: (
+    <img src="https://symphony.com/wp-content/uploads/2019/06/logo.png" />
+  ),
+};
