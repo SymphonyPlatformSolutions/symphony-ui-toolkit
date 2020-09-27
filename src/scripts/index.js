16f8dd655dc804fbcabad725aae8533dba853b1c
Anna Llorens <anna.llorens@symphony.com>
2020-09-27 17:30:31 +0200

    Update script

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,41 +1,25 @@
 
-var fs = require('fs');
+const fs = require('fs');
 const { execSync } = require('child_process');
 
+const DIST_FONTS = 'dist/fonts/';
+const SRC_ICONS ='src/icons/';
 
-const ICONS_SCSS = `_tk-icons-embedded.scss`;
-const OUTPUT_FONTS_FOLDER = 'src/assets/fonts/';
-const ICONS_FOLDER = 'src/icons/';
-const ICONS_HTML = `${OUTPUT_FONTS_FOLDER}tk-icons.html`;
-const INPUT_SVG_SOURCES = `${ICONS_FOLDER}svg/*.svg`;
-const OUTPUT_SCSS = `${ICONS_FOLDER}${ICONS_SCSS}`;
-const TEMP_SCSS = `dist/fonts/${ICONS_SCSS}`;
-
-const build = () => {
-  execSync(`mkdir -p dist dist/fonts`);
-  fs.copyFileSync(OUTPUT_SCSS, TEMP_SCSS);
-  execSync(`icon-font-generator ${INPUT_SVG_SOURCES} -o dist/fonts/ --csspath ${OUTPUT_SCSS} -p tk-icon -n tk-icons -j false --html false`);
-  execSync(`yarn sass --load-path=node_modules --no-source-map --style compressed uitoolkit.scss:dist/css/uitoolkit.css uitoolkit-tempo-support.scss:dist/css/uitoolkit-tempo-support.css`);
-  fs.copyFileSync(TEMP_SCSS, OUTPUT_SCSS);
-  execSync(`rm -rf ${TEMP_SCSS}`);
+const generateFonts = () => {
+  execSync(`mkdir -p dist ${DIST_FONTS}`);
+  execSync(`icon-font-generator ${SRC_ICONS}svg/*.svg -o ${DIST_FONTS} --csspath ${DIST_FONTS}tk-icons-definitions.scss --csstp ${SRC_ICONS}tk-icons-template.hbs -p tk-icon -n tk-icons`, {stdio: 'inherit'});
 } 
 
-const iconsStorybook = () => {
-  execSync(`icon-font-generator ${INPUT_SVG_SOURCES} -o ${OUTPUT_FONTS_FOLDER} --csspath ${OUTPUT_SCSS} -p tk-icon -n tk-icons -j false --csstp ${OUTPUT_FONTS_FOLDER}tk-icons.hbs`,  {stdio: 'inherit'});
-  fs.readFileSync(ICONS_HTML,'utf8', (err, data) =>  {
+const generateIcons = () => {
+  generateFonts();
+  fs.readFile(`${DIST_FONTS}tk-icons.html`,'utf8',function (err,data) {
     const iconsHtml = data.substring(data.indexOf('<body>')+6, data.indexOf('</body>'));
-    const iconsStoryBook = "export default { title: 'Icons'}; export const Icon = () => `"+iconsHtml+"`"
-    fs.writeFileSync('stories/icons.stories.js', iconsStoryBook);
+    const iconsStorybook = "export default { title: 'Icons'}; export const Icon = () => `"+iconsHtml+"`";
+    fs.writeFileSync('stories/icons.stories.js', iconsStorybook);
   });
-  execSync(`rm -rf ${ICONS_HTML}`);
   console.info('Auto-generated icons.stories.js ✅');    
 }
 
-if (process.argv.includes('build')) { 
-  build();
+if (process.argv.includes('icons')) {
+  generateIcons();
 }
-if (process.argv.includes('iconsStorybook')) {
-  iconsStorybook();
-}
-
-
