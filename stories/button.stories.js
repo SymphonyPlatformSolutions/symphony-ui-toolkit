87c9e3b7957ddd9668f89fdf1ba602b8acca4a5b
Anna Llorens <anna.llorens@symphony.com>
2020-09-28 10:35:04 +0200

    Update stories

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -7,7 +7,7 @@ export const Button = () => `
   <h3>Primary button</h3>
   <button class="tk-button tk-button--primary">Button</button>
   <button class="tk-button tk-button--primary"><i class="tk-icon-lock"></i> Button</button>
-  <button class="tk-button tk-button--primary tk-button--icon"><i class="tk-icon-more-_"></i></button>
+  <button class="tk-button tk-button--primary tk-button--icon"><i class="tk-icon-more"></i></button>
   <button class="tk-button tk-button--primary" disabled>Button</button>
   <button class="tk-button tk-button--primary" disabled><i class="tk-icon-lock"></i> Button</button>
   <button class="tk-button tk-button--primary" disabled><i class="tk-icon-check tk-color-action-ok"></i></button>
@@ -17,7 +17,7 @@ export const Button = () => `
   <h3>Secondary button</h3>
   <button class="tk-button tk-button--secondary">Button</button>
   <button class="tk-button tk-button--secondary"><i class="tk-icon-lock"></i> Button</button>
-  <button class="tk-button tk-button--secondary tk-button--icon"><i class="tk-icon-more-_"></i></button>
+  <button class="tk-button tk-button--secondary tk-button--icon"><i class="tk-icon-more"></i></button>
   <button class="tk-button tk-button--secondary" disabled>Button</button>
   <button class="tk-button tk-button--secondary" disabled><i class="tk-icon-lock"></i> Button</button>
   <button class="tk-button tk-button--secondary" disabled><i class="tk-icon-check tk-color-action-ok"></i></button>
@@ -27,7 +27,7 @@ export const Button = () => `
   <h3>Tertiary button</h3>
   <button class="tk-button tk-button--tertiary">Button</button>
   <button class="tk-button tk-button--tertiary"><i class="tk-icon-lock"></i> Button</button>
-  <button class="tk-button tk-button--tertiary tk-button--icon"><i class="tk-icon-more-_"></i></button>
+  <button class="tk-button tk-button--tertiary tk-button--icon"><i class="tk-icon-more"></i></button>
   <button class="tk-button tk-button--tertiary" disabled>Button</button>
   <button class="tk-button tk-button--tertiary" disabled><i class="tk-icon-lock"></i> Button</button>
   <button class="tk-button tk-button--tertiary" disabled><i class="tk-icon-check tk-color-action-ok"></i></button>
@@ -37,7 +37,7 @@ export const Button = () => `
   <h3>Destructive button</h3>
   <button class="tk-button tk-button--destructive">Button</button>
   <button class="tk-button tk-button--destructive"><i class="tk-icon-lock"></i> Button</button>
-  <button class="tk-button tk-button--destructive tk-button--icon"><i class="tk-icon-more-_"></i></button>
+  <button class="tk-button tk-button--destructive tk-button--icon"><i class="tk-icon-more"></i></button>
   <button class="tk-button tk-button--destructive" disabled>Button</button>
   <button class="tk-button tk-button--destructive" disabled><i class="tk-icon-lock"></i> Button</button>
   <button class="tk-button tk-button--destructive" disabled><i class="tk-icon-check tk-color-action-ok"></i></button>
