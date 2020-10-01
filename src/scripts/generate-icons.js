fa8c759678b9b13d1b545798769334f519eb3c2f
Adnane Tellou <63653674+symphony-adnane@users.noreply.github.com>
2020-10-01 15:22:35 +0200

    APP-3040  Improvements on icons, buttons, checkbox (#47)
    * APP-3040  Improvements on icons, buttons, checkbox
    
    * add comments to icons template

---
+++ src/scripts/generate-icons.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const { execSync } = require('child_process');
+
+const DIST_FONTS = 'dist/fonts/';
+const SRC_ICONS = 'src/icons/';
+const GENERATE_ICON_CMD = `icon-font-generator -s ${SRC_ICONS}svg/*.svg -o ${DIST_FONTS} --csspath ${DIST_FONTS}tk-icons-definitions.scss --csstp ${SRC_ICONS}tk-icons-template.hbs -p tk-icon -n tk-icons --mono --center --height 128`;
+
+const generateFonts = () => {
+  execSync(`mkdir -p dist ${DIST_FONTS}`);
+  execSync(GENERATE_ICON_CMD, { stdio: 'inherit' });
+};
+
+const generateIcons = () => {
+  generateFonts();
+  fs.readFile(`${DIST_FONTS}tk-icons.html`, 'utf8', function(err, data) {
+    const iconsHtml = data.substring(data.indexOf('<body>') + 6, data.indexOf('</body>'));
+    const iconsStorybook =
+      "export default { title: 'Icons'}; export const Icon = () => `" + iconsHtml + '`';
+    fs.writeFileSync('stories/icons.stories.js', iconsStorybook);
+  });
+  console.info('Auto-generated icons.stories.js ✅');
+};
+
+generateIcons();
--- stories/checkbox.stories.js
+++ stories/checkbox.stories.js
@@ -1,5 +1,5 @@
 export default {
-  title: "Checkbox",
+  title: 'Checkbox',
 };
 
 export const Checkbox = () => {
@@ -11,7 +11,7 @@ export const Checkbox = () => {
         <div>
             <span class="tk-checkbox" tabindex="0">
                 <div class="tk-checkbox__inputContainer" tab-index="-1">
-                    <input class="tk-checkbox__input"
+                    <input class="tk-checkbox__input" 
                         type="checkbox" id="checkbox-5WuJWaDfz" name="simple-checkbox" tabindex="-1"
                         value="checkbox-1">
                     <span class="tk-checkbox__icon" aria-hidden></span>
@@ -25,19 +25,19 @@ export const Checkbox = () => {
                 <div class="tk-checkbox__inputContainer" tab-index="-1">
                     <input class="tk-checkbox__input"
                         type="checkbox" id="checkbox-_KdZz3_g72" name="active-checkbox" tabindex="-1"
-                        value="active-checkbox-1">
-                    <span class="tk-checkbox__icon tk-checkbox__icon--on" aria-hidden></span>
+                        value="active-checkbox-1" checked>
+                    <span class="tk-checkbox__icon" aria-hidden></span>
                 </div>
                 <label class="tk-checkbox__label" for="checkbox-_KdZz3_g72" tabindex="-1">Checkbox 'on'</label>
             </span>
         </div>
         <div>
-            <span class="tk-checkbox" tabindex="0">
+            <span class="tk-checkbox tk-checkbox--indeterminate" tabindex="0">
                 <div class="tk-checkbox__inputContainer" tab-index="-1">
                     <input class="tk-checkbox__input"
                         type="checkbox" id="checkbox-W13s-EPfKu" name="active-checkbox" tabindex="-1"
                         value="active-checkbox-2">
-                    <span class="tk-checkbox__icon tk-checkbox__icon--indeterminate" aria-hidden></span>
+                    <span class="tk-checkbox__icon" aria-hidden></span>
                 </div>
                 <label class="tk-checkbox__label" for="checkbox-W13s-EPfKu"
                     tabindex="-1">Checkbox 'indeterminate'</label>
@@ -49,7 +49,7 @@ export const Checkbox = () => {
                     <input class="tk-checkbox__input"
                         type="checkbox" id="checkbox-rWqEZbrVP1" name="active-checkbox" tabindex="-1"
                         value="active-checkbox-3">
-                    <span class="tk-checkbox__icon tk-checkbox__icon--off" aria-hidden></span>
+                    <span class="tk-checkbox__icon" aria-hidden></span>
                 </div>
                 <label class="tk-checkbox__label" for="checkbox-rWqEZbrVP1"
                     tabindex="-1">Checkbox 'off'</label>
@@ -64,21 +64,21 @@ export const Checkbox = () => {
                 <div class="tk-checkbox__inputContainer" tab-index="-1">
                     <input class="tk-checkbox__input"
                         type="checkbox" id="checkbox-x8ESD33S3W" name="disabled-checkbox" disabled="" tabindex="-1"
-                        value="disabled-checkbox-1">
-                    <span class="tk-checkbox__icon tk-checkbox__icon--on" aria-hidden></span>
+                        value="disabled-checkbox-1" checked>
+                    <span class="tk-checkbox__icon" aria-hidden></span>
                 </div>
                 <label class="tk-checkbox__label" for="checkbox-x8ESD33S3W"
                     tabindex="-1">Checkbox</label>
             </span>
         </div>
         <div>
-            <span class="tk-checkbox tk-checkbox--disabled"
+            <span class="tk-checkbox tk-checkbox--indeterminate tk-checkbox--disabled"
                 tabindex="0">
                 <div class="tk-checkbox__inputContainer" tab-index="-1">
                     <input class="tk-checkbox__input"
                         type="checkbox" id="checkbox-Y6ospnNQOs" name="disabled-checkbox" disabled="" tabindex="-1"
                         value="disabled-checkbox-2">
-                    <span class="tk-checkbox__icon tk-checkbox__icon--indeterminate" aria-hidden></span>
+                    <span class="tk-checkbox__icon" aria-hidden></span>
                 </div>
                 <label class="tk-checkbox__label" for="checkbox-Y6ospnNQOs"
                     tabindex="-1">Checkbox</label>
@@ -91,7 +91,7 @@ export const Checkbox = () => {
                     <input class="tk-checkbox__input"
                         type="checkbox" id="checkbox-2rgjiQCh_S" name="disabled-checkbox" disabled="" tabindex="-1"
                         value="disabled-checkbox-3">
-                    <span class="tk-checkbox__icon tk-checkbox__icon--off" aria-hidden></span>
+                    <span class="tk-checkbox__icon" aria-hidden></span>
                 </div>
                 <label class="tk-checkbox__label" for="checkbox-2rgjiQCh_S"
                     tabindex="-1">Checkbox</label>
@@ -101,44 +101,44 @@ export const Checkbox = () => {
     <div>
         <h2>Label placements</h2>
         <p>The label can be positioned at the <strong>top, right, bottom, left</strong> of the checkbox</p>
-        <div class="tk-checkbox__container">
+        <div class="d-inline-block">
             <span class="tk-checkbox tk-checkbox__labelPlacement--top" tabindex="0">
                 <div class="tk-checkbox__inputContainer" tab-index="-1">
                     <input class="tk-checkbox__input" type="checkbox"
                         id="checkbox-7_oZWaQUH0" name="placement-label" tabindex="-1" value="top">
-                    <span class="tk-checkbox__icon tk-checkbox__icon--off" aria-hidden></span>
+                    <span class="tk-checkbox__icon" aria-hidden></span>
                 </div>
             <label class="tk-checkbox__label tk-checkbox__label--top" for="checkbox-7_oZWaQUH0" tabindex="-1">Top</label>
             </span>
         </div>
-        <div class="tk-checkbox__container">
+        <div class="d-inline-block">
             <span class="tk-checkbox tk-checkbox__labelPlacement--left" tabindex="0">
                 <div class="tk-checkbox__inputContainer" tab-index="-1">
                     <input class="tk-checkbox__input" type="checkbox"
                         id="checkbox-LJE9KbEip4" name="placement-label" tabindex="-1" value="left">
                     <span
-                        class="tk-checkbox__icon tk-checkbox__icon--off" aria-hidden></span>
+                        class="tk-checkbox__icon" aria-hidden></span>
                 </div>
                 <label class="tk-checkbox__label tk-checkbox__label--left" for="checkbox-LJE9KbEip4" tabindex="-1">Left</label>
             </span>
         </div>
-        <div class="tk-checkbox__container">
+        <div class="d-inline-block">
             <span class="tk-checkbox tk-checkbox__labelPlacement--bottom" tabindex="0">
                 <div class="tk-checkbox__inputContainer" tab-index="-1">
                     <input class="tk-checkbox__input" type="checkbox"
                         id="checkbox-krlkk7ughb" name="placement-label" tabindex="-1" value="bottom">
-                    <span class="tk-checkbox__icon tk-checkbox__icon--off" aria-hidden></span>
+                    <span class="tk-checkbox__icon" aria-hidden></span>
                 </div>
                 <label class="tk-checkbox__label tk-checkbox__label--bottom" for="checkbox-krlkk7ughb"
                     tabindex="-1">bottom</label>
             </span>
         </div>
-        <div class="tk-checkbox__container">
+        <div class="d-inline-block">
             <span class="tk-checkbox tk-checkbox__labelPlacement--right" tabindex="0">
                 <div class="tk-checkbox__inputContainer" tab-index="-1">
                     <input class="tk-checkbox__input" type="checkbox"
                         id="checkbox-CjTivG3wCX" name="placement-label" tabindex="-1" value="right">
-                    <span class="tk-checkbox__icon tk-checkbox__icon--off" aria-hidden></span>
+                    <span class="tk-checkbox__icon" aria-hidden></span>
                 </div>
                 <label class="tk-checkbox__label tk-checkbox__label--right" for="checkbox-CjTivG3wCX"
                     tabindex="-1">Right</label>
