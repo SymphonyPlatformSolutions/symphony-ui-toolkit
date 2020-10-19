f32e3e40c08e12cc9ac66b854e381c7cb719b28a
Anna Llorens <anna.llorens@symphony.com>
2020-10-19 11:21:23 +0200

    APP-3168 Added missing icon

--- stories/version.stories.js
+++
@@ -1,8 +0,0 @@
-import * as pck from '../package.json';
-export default {
-  title: 'Version',
-};
-
-export const Version = () => `
-  <h2>Current version <a class="tk-link" target="_blank" href="https://github.com/SymphonyPlatformSolutions/symphony-bdk-ui-toolkit-styles/releases/tag/v${pck.version}">${pck.version}</a></h2>
-  `;
