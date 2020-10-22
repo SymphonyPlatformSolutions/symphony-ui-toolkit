659d89d6ff784c5a9760be6a415894c07d096c07
Anna Llorens <anna.llorens@symphony.com>
2020-10-22 12:12:11 +0200

    APP-3001 Fix ut

--- spec/components/typography/Typography.spec.tsx
+++ spec/components/typography/Typography.spec.tsx
@@ -6,7 +6,7 @@ describe('Typography Component', () => {
   describe('Typography component test suite => ', () => {
     it('should render the component with default props', () => {
       const wrapper = shallow(<Typography>Body text</Typography>);
-      expect(wrapper.hasClass('tk-typography tk-typography--span')).toBe(true);
+      expect(wrapper.hasClass('tk-typography tk-typography--body')).toBe(true);
     });
     it('should render extra props to the typography component', () => {
       const wrapper = shallow(<Typography type="h1" variant="bold">Close me</Typography>);
