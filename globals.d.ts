926536a286b5874f1bfaf129faa5f7fddb33ac70
Anna Llorens <anna.llorens@symphony.com>
2020-10-26 14:37:43 +0100

    APP-3124 Fix lint

--- globals.d.ts
+++ globals.d.ts
@@ -1,1 +1,2 @@
+// eslint-disable-next-line
 declare module "*.md";
--- stories/Docs.stories.tsx
+++ stories/Docs.stories.tsx
@@ -1,13 +1,12 @@
+import React from 'react';
+import GettingStartedDoc from '../docs/getting-started.md';
 
-import React from "react";
-import gettingStarted from '../docs/getting-started.md';
-
-const parse = require('html-react-parser');
+import ReactHtmlParser from 'html-react-parser';
 
 export default {
   title: 'Docs',
 };
 
 export const GettingStarted: React.SFC = () => {
-  return (<div className="tk-ml-2h">{parse(gettingStarted)}</div>)
+  return (<div className="tk-ml-2h">{ReactHtmlParser(GettingStartedDoc)}</div>)
 };
