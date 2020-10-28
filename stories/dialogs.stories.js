97637960954429343575540bb399bdc2b6179687
Anna Llorens <anna.llorens@symphony.com>
2020-10-28 17:38:04 +0100

    APP-2972 Added dialog styles

---
+++ stories/dialogs.stories.js
@@ -0,0 +1,91 @@
+export default {
+  title: 'Dialogs',
+};
+
+export const smallDialog = () => `
+<div class="tk-dialog-backdrop">
+  <div class="tk-dialog tk-dialog__small">
+    <div class="tk-dialog__header">
+      <div class="tk-typography tk-typography--h1">Modal title</div>
+      <button class="tk-dialog__close">
+        <i class="tk-icon-cross"></i>
+      </button>
+    </div>
+    <div class="tk-dialog__body">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
+    <div class="tk-dialog__footer">
+      <button class="tk-button tk-button--tertiary">Cancel</button>
+      <button class="tk-button tk-button--primary">Confirm</button>
+    </div>
+  </div>
+</div>
+`;
+
+export const mediumDialog = () => `
+<div class="tk-dialog-backdrop">
+  <div class="tk-dialog tk-dialog__medium">
+    <div class="tk-dialog__header">
+      <div class="tk-typography tk-typography--h1">Modal title</div>
+      <button class="tk-dialog__close">
+        <i class="tk-icon-cross"></i>
+      </button>
+    </div>
+    <div class="tk-dialog__body">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
+    <div class="tk-dialog__footer">
+      <button class="tk-button tk-button--tertiary">Cancel</button>
+      <button class="tk-button tk-button--primary">Confirm</button>
+    </div>
+  </div>
+</div>
+`;
+
+
+export const largeDialog = () => `
+<div class="tk-dialog-backdrop">
+  <div class="tk-dialog tk-dialog__large">
+    <div class="tk-dialog__header">
+      <div class="tk-typography tk-typography--h1">Modal title</div>
+      <button class="tk-dialog__close">
+        <i class="tk-icon-cross"></i>
+      </button>
+    </div>
+    <div class="tk-dialog__body">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
+    <div class="tk-dialog__footer">
+      <button class="tk-button tk-button--tertiary">Cancel</button>
+      <button class="tk-button tk-button--primary">Confirm</button>
+    </div>
+  </div>
+</div>
+`;
