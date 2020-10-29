cb1671b9608d419cd8662185ccde4299e4fa027c
Anna Llorens <anna.llorens@symphony.com>
2020-10-29 18:26:59 +0100

    APP-2972 Update dialog styles

--- stories/dialogs.stories.js
+++ stories/dialogs.stories.js
@@ -4,12 +4,10 @@ export default {
 
 export const smallDialog = () => `
 <div class="tk-dialog-backdrop">
-  <div class="tk-dialog tk-dialog__small">
+  <div class="tk-dialog tk-dialog--small">
     <div class="tk-dialog__header">
-      <div class="tk-typography tk-typography--h1">Modal title</div>
-      <button class="tk-dialog__close">
-        <i class="tk-icon-cross"></i>
-      </button>
+      <div class="tk-dialog__title">Modal title</div>
+      <button class="tk-dialog__close"></button>
     </div>
     <div class="tk-dialog__body">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
@@ -33,12 +31,10 @@ export const smallDialog = () => `
 
 export const mediumDialog = () => `
 <div class="tk-dialog-backdrop">
-  <div class="tk-dialog tk-dialog__medium">
+  <div class="tk-dialog tk-dialog--medium">
     <div class="tk-dialog__header">
       <div class="tk-typography tk-typography--h1">Modal title</div>
-      <button class="tk-dialog__close">
-        <i class="tk-icon-cross"></i>
-      </button>
+      <button class="tk-dialog__close"></button>
     </div>
     <div class="tk-dialog__body">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
@@ -63,14 +59,20 @@ export const mediumDialog = () => `
 
 export const largeDialog = () => `
 <div class="tk-dialog-backdrop">
-  <div class="tk-dialog tk-dialog__large">
+  <div class="tk-dialog tk-dialog--large">
     <div class="tk-dialog__header">
-      <div class="tk-typography tk-typography--h1">Modal title</div>
-      <button class="tk-dialog__close">
-        <i class="tk-icon-cross"></i>
-      </button>
+      <div class="tk-dialog__title">Modal title</div>
+      <button class="tk-dialog__close"></button>
     </div>
     <div class="tk-dialog__body">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+      dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
       quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -86,6 +88,82 @@ export const largeDialog = () => `
       <button class="tk-button tk-button--tertiary">Cancel</button>
       <button class="tk-button tk-button--primary">Confirm</button>
     </div>
+  </div>
+</div>
+`;
+export const fullPageDialog = () => `
+<div class="tk-dialog-backdrop">
+  <div class="tk-dialog tk-dialog--full-width">
+    <div class="tk-dialog__header">
+      <div class="tk-dialog__title">Modal title</div>
+      <button class="tk-dialog__close"></button>
+    </div>
+    <div class="tk-dialog__body">
+    orem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      orem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      orem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      orem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
+    <div class="tk-dialog__footer">
+      <button class="tk-button tk-button--tertiary">Cancel</button>
+      <button class="tk-button tk-button--primary">Confirm</button>
+    </div>
+  </div>
+</div>
+`;
+export const SimpleDialog = () => `
+<div class="tk-dialog-backdrop">
+  <div class="tk-dialog">
+    <div class="tk-dialog__header">
+      <button class="tk-dialog__close"></button>
+    </div>  
+    <div class="tk-dialog__body">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididu
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip.
+    </div>
+    <div class="tk-dialog__footer"></div>
   </div>
 </div>
 `;
