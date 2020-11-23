955d04671dfa1a4ce65beb7eea3d9f352b47bed7
Ting Liu <liuting901014@gmail.com>
2020-11-23 18:21:47 +0100

    Implement Avatar component

---
+++ spec/components/avatar/Avatar.spec.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { Avatar, BasicIndicator } from '../../../src/components/avatar';
+import { mount } from 'enzyme';
+
+describe('Avatar', () => {
+  it('should render the content with correct class and without crash', () => {
+    const wrapper = mount(
+      <Avatar size='small' variant='square'>
+        <span style={{fontSize:"10px"}}>AB</span>
+      </Avatar>
+    )
+    expect(wrapper.find('Avatar').length).toBe(1);
+    expect(wrapper.find('div').hasClass('tk-avatar tk-avatar--small tk-avatar--square')).toBe(true)
+  })
+
+  it('should render indicator with the correct position class', () => {
+    const wrapper = mount(
+      <Avatar variant='round' size='xlarge'>
+        <img src={'some url'} alt="avatar"/>
+        <BasicIndicator position='top' variant='attention'/>
+      </Avatar>
+    )
+    expect(wrapper.find('BasicIndicator').length).toBe(1);
+    expect(wrapper.find('span').hasClass('tk-badge tk-badge--top tk-bg-color--attention')).toBe(true);
+  })
+})
---
+++ src/components/avatar/Avatar.tsx
@@ -0,0 +1,72 @@
+import * as React from 'react'
+import classNames from 'classnames';
+
+type AvatarProps = {
+  size: 'xxlarge' | 'xlarge' | 'large' | 'medium' | 'small' | 'xsmall';
+  bordered?: boolean;
+  variant: 'square' | 'round';
+  children?: React.ReactNode;
+  className?: string;
+}
+
+type AvatarBadgeProps = {
+  position: 'top' | 'bottom';
+  children?: React.ReactNode;
+  className?: string;
+}
+
+type BasicIndicatorProps = {
+  position: 'top' | 'bottom';
+  variant: 'attention' | 'ok' | 'offline' | 'warning';
+}
+const prefix = 'tk-avatar'
+
+export const BasicIndicator: React.FC<BasicIndicatorProps> = ({position, variant}: BasicIndicatorProps) => {
+  const classes = classNames(
+    'tk-badge',
+    `tk-badge--${position}`,
+    `tk-bg-color--${variant}`,
+  )
+  return (
+    <span
+      className={classes}
+      style={{width:"30%", height:"30%", borderRadius:"50%"}}
+    >
+    </span>
+  )
+}
+
+export const AvatarBadge: React.FC<AvatarBadgeProps> = ({position, children, className}: AvatarBadgeProps) => {
+  const classes = classNames(
+    'tk-badge',
+    `tk-badge--${position}`,
+    className
+
+  )
+  return (
+    <span className={classes}>
+      {children}
+    </span>
+  )
+}
+
+export const Avatar: React.FC<AvatarProps> = ({
+  size,
+  bordered,
+  variant,
+  children,
+  className,
+}: AvatarProps) => {
+  const classes = classNames(
+    prefix,
+    `${prefix}--${size}`,
+    {[`${prefix}--border`]: bordered},
+    {[`${prefix}--square`]: variant === 'square'},
+    className
+  )
+  return (
+    <div className={classes}>
+      {children}
+    </div>
+  )
+}
