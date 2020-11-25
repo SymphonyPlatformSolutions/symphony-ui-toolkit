3b1fba0bb6b94c0def85a436ec12e3691f27f30c
Axel Eriksson <60875212+axeleriksson147@users.noreply.github.com>
2020-11-25 10:22:55 +0100

    APP-3294 - Moved current .tk-tooltip styling to .tk-hint and updated .tk-tooltip (#29)
    * Moved current .tk-tooltip styling to .tk-hint and updated .tk-tooltip
    
    * Split up into Hint and Tooltip separate files for styling and story
    
    * Added hint import

--- stories/tooltip.stories.js
+++ stories/tooltip.stories.js
@@ -7,13 +7,9 @@ export const Tooltip = () => {
     <div style="min-height: 600px">
       <h2>Tooltip</h2>
         <div id="hint-vZUXsOqChv" role="tooltip" class="tk-tooltip" style="max-width: 400px; position: relative">
-            <span class="tk-tooltip__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
-            <div style="position: absolute; left: calc(50% - 10px); bottom: -32px;">
-                <div class="tk-tooltip__arrow" style="transform: rotate(45deg);"></div>       
-            </div>
-            <div class="tk-tooltip__footer">
-                <span class="tk-tooltip__close" style="cursor: pointer">Got it</span>
-            </div>
+            <span class="tk-tooltip__description">
+                Tooltip
+            </span>
         </div>
       </div>
     `);
