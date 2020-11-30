0b89e168a82efd94f66511bcafa68a4aca6bdc85
Adnane <adnane.tellou@symphony.com>
2020-11-30 17:19:09 +0100

    fix ut

--- spec/components/modal/Modal.spec.tsx
+++ spec/components/modal/Modal.spec.tsx
@@ -6,7 +6,7 @@ import { ModalBody, ModalHeader } from '../../../src/components/modal/Modal';
 describe('Modal', () => {
   it('should render the content with correct class and without crash', () => {
     const wrapper = mount(
-      <Modal size={'small'}>
+      <Modal size={'small'} show>
         <ModalHeader>Hello, World</ModalHeader>
         <ModalBody>Some text</ModalBody>
       </Modal>
@@ -18,5 +18,17 @@ describe('Modal', () => {
     expect(wrapper.find('ModalHeader').children().hasClass('tk-dialog__header')).toBe(true);
     expect(wrapper.find('ModalBody').text()).toBe('Some text');
     expect(wrapper.find('ModalBody').children().hasClass('tk-dialog__body')).toBe(true);
+  })
+  it('should render nothing', () => {
+    const wrapper = mount(
+      <Modal size={'small'}>
+        <ModalHeader>Hello, World</ModalHeader>
+        <ModalBody>Some text</ModalBody>
+      </Modal>
+    );
+
+    expect(wrapper.find('Modal').children().length).toBe(0);
+    expect(wrapper.find('ModalHeader').length).toBe(0);
+    expect(wrapper.find('ModalBody').length).toBe(0);
   })
 });
