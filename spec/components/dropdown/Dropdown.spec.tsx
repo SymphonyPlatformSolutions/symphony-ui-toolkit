28d367f319adf25dee59d00380f1d949d594ea9b
Anna Llorens <anna.llorens@symphony.com>
2021-01-14 16:56:24 +0100

    APP-2986 Enhance dropdown storypage

--- spec/components/dropdown/Dropdown.spec.tsx
+++ spec/components/dropdown/Dropdown.spec.tsx
@@ -4,101 +4,100 @@ import { render, screen } from '@testing-library/react';
 import Dropdown from '../../../src/components/dropdown';
 import { DropdownProps } from '../../../src/components/dropdown/Dropdown';
 import '@testing-library/jest-dom/extend-expect';
-import { customRenderOptions, IconPicker, MultiValueRemove } from '../../../src/components/dropdown/CustomRender';
+import { customRenderOptions, IconPicker } from '../../../src/components/dropdown/CustomRender';
 
 describe('Dropdown component test suite =>', () => {
-	let dropdownProps: DropdownProps = {
-		options: [],
-		id: 'testId'
-	};
+  const dropdownProps: DropdownProps = {
+    options: [],
+    id: 'testId'
+  };
 
-	beforeEach(() => {
-		dropdownProps.options = [ { label: 'banana' }, { label: 'avocado' }, { label: 'orange' } ];
-	});
+  beforeEach(() => {
+    dropdownProps.options = [ { label: 'banana' }, { label: 'avocado' }, { label: 'orange' } ];
+  });
 
-	describe('when is simple Dropdown', () => {
-		it('should render the Dropdown component by default', async () => {
-			const { getByText } = render(<Dropdown options={dropdownProps.options} />);
-			expect(getByText('Select...')).toBeInTheDocument();
+  describe('when is simple Dropdown', () => {
+    it('should render the Dropdown component by default', async () => {
+      const { getByText } = render(<Dropdown options={dropdownProps.options} />);
+      expect(getByText('Select...')).toBeInTheDocument();
     });
     
-		it('should show/hide options menu', async () => {
-			const { container, getByText } = render(<Dropdown options={dropdownProps.options} />);
-			const input = screen.getByRole('textbox');
-			userEvent.click(input);
-			expect(getByText('banana')).toBeTruthy();
-			expect(getByText('avocado')).toBeTruthy();
-			expect(getByText('orange')).toBeTruthy();
+    it('should show/hide options menu', async () => {
+      const { getByText } = render(<Dropdown options={dropdownProps.options} />);
+      const input = screen.getByRole('textbox');
+      userEvent.click(input);
+      expect(getByText('banana')).toBeTruthy();
+      expect(getByText('avocado')).toBeTruthy();
+      expect(getByText('orange')).toBeTruthy();
     });
     
-		it('should select first option', async () => {
-			const { getByText } = render(<Dropdown options={dropdownProps.options} />);
-			const input = screen.getByRole('textbox');
-			userEvent.click(input);
-			const option = screen.getByText('banana');
-			userEvent.click(option);
-			expect(getByText('banana')).toBeTruthy();
+    it('should select first option', async () => {
+      const { getByText } = render(<Dropdown options={dropdownProps.options} />);
+      const input = screen.getByRole('textbox');
+      userEvent.click(input);
+      const option = screen.getByText('banana');
+      userEvent.click(option);
+      expect(getByText('banana')).toBeTruthy();
     });
     
-		it('should render costum render dropdown', async () => {
-			const { getByText } = render(
-				<Dropdown
-					isMultiSelect
-					options={customRenderOptions}
-					optionRenderer={IconPicker}
-					displayArrowIndicator
-					tagRenderer={IconPicker}
-				/>
-			);
-			const input = screen.getByRole('textbox');
-			userEvent.click(input);
-			const option = screen.getByText('app');
-			userEvent.click(option);
-			expect(getByText('app')).toBeTruthy();
-		});
-	});
+    it('should render costum render dropdown', async () => {
+      const { getByText } = render(
+        <Dropdown
+          isMultiSelect
+          options={customRenderOptions}
+          optionRenderer={IconPicker}
+          displayArrowIndicator
+          tagRenderer={IconPicker}
+        />
+      );
+      const input = screen.getByRole('textbox');
+      userEvent.click(input);
+      const option = screen.getByText('app');
+      userEvent.click(option);
+      expect(getByText('app')).toBeTruthy();
+    });
+  });
 
-	describe('when is Multiselect', () => {
+  describe('when is Multiselect', () => {
 
-		it('should render the Multiselect component by default', async () => {
-			const { getByText } = render(<Dropdown options={dropdownProps.options} isMultiSelect />);
-			expect(getByText('Select...')).toBeInTheDocument();
+    it('should render the Multiselect component by default', async () => {
+      const { getByText } = render(<Dropdown options={dropdownProps.options} isMultiSelect />);
+      expect(getByText('Select...')).toBeInTheDocument();
     });
     
-		it('should show/hide options menu', async () => {
-			const { getByText } = render(<Dropdown options={dropdownProps.options} isMultiSelect />);
-			const input = screen.getByRole('textbox');
-			userEvent.click(input);
-			expect(getByText('banana')).toBeTruthy();
-			expect(getByText('avocado')).toBeTruthy();
-			expect(getByText('orange')).toBeTruthy();
+    it('should show/hide options menu', async () => {
+      const { getByText } = render(<Dropdown options={dropdownProps.options} isMultiSelect />);
+      const input = screen.getByRole('textbox');
+      userEvent.click(input);
+      expect(getByText('banana')).toBeTruthy();
+      expect(getByText('avocado')).toBeTruthy();
+      expect(getByText('orange')).toBeTruthy();
     });
     
-		it('should select first option', async () => {
-			const { getByText } = render(<Dropdown options={dropdownProps.options} isMultiSelect />);
-			const input = screen.getByRole('textbox');
-			userEvent.click(input);
-			const option = screen.getByText('banana');
-			userEvent.click(option);
-			expect(getByText('banana')).toBeTruthy();
+    it('should select first option', async () => {
+      const { getByText } = render(<Dropdown options={dropdownProps.options} isMultiSelect />);
+      const input = screen.getByRole('textbox');
+      userEvent.click(input);
+      const option = screen.getByText('banana');
+      userEvent.click(option);
+      expect(getByText('banana')).toBeTruthy();
     });
     
-		it('should render costum render dropdown', async () => {
-			const { getByText } = render(
-				<Dropdown
-					isMultiSelect
-					isTagClearable={false}
-					options={customRenderOptions}
-					optionRenderer={IconPicker}
-					displayArrowIndicator
-					tagRenderer={IconPicker}
-				/>
-			);
-			const input = screen.getByRole('textbox');
-			userEvent.click(input);
-			const option = screen.getByText('app');
-			userEvent.click(option);
-			expect(getByText('app')).toBeTruthy();
-		});
-	});
+    it('should render costum render dropdown', async () => {
+      const { getByText } = render(
+        <Dropdown
+          isMultiSelect
+          options={customRenderOptions}
+          optionRenderer={IconPicker}
+          displayArrowIndicator
+          tagRenderer={IconPicker}
+        />
+      );
+      const input = screen.getByRole('textbox');
+      userEvent.click(input);
+      const option = screen.getByText('app');
+      userEvent.click(option);
+      expect(getByText('app')).toBeTruthy();
+    });
+  });
 });
--- src/components/dropdown/CustomRender.tsx
+++ src/components/dropdown/CustomRender.tsx
@@ -2,6 +2,7 @@ import Icon from '../icon';
 import * as React from 'react';
 import  { components } from 'react-select';
 import * as PropTypes from 'prop-types';
+import { CustomRenderProps, DefaultOptions } from './Dropdown';
 
 export const Option = props => {
   const OptionRender = props?.selectProps?.optionRenderer;
@@ -44,15 +45,14 @@ MultiValue.propTypes = {
 
 export const MultiValueRemove = props => {
   const TagRemove = props.selectProps.tagRemoveRenderer;
-  return (<div className="tk-d-flex"> {(TagRemove && !!props.selectProps.isTagClearable) ?
+  return (<div className="tk-d-flex"> {(TagRemove ?
     <div className="tk-d-flex tk-tag-remove">
       <components.MultiValueRemove {...props} >
-        <TagRemove  {...props} className="tk-tag-remove" />
+        <TagRemove {...props} className="tk-tag-remove"/>
       </components.MultiValueRemove> </div> :
-    (<div className="tk-d-flex"> { props.selectProps.isTagClearable ?
-      <components.MultiValueRemove {...props}>
-        <Icon iconName="cross" />
-      </components.MultiValueRemove> : <div className="tk-d-none" />}</div>)}</div>
+    <components.MultiValueRemove {...props}>
+      <Icon iconName="cross" />
+    </components.MultiValueRemove>)}</div>
   );
 };
 
@@ -76,10 +76,36 @@ DropdownIndicator.propTypes = {
 
 export const Control = props => {
   return (<div className="tk-input-group__header">
-    {<label className="tk-label">{props?.selectProps?.label}</label>}
+    {<label className="tk-label tk-mb-h">{props?.selectProps?.label}</label>}
     <components.Control {...props} /></div>);
 }
 
 Control.propTypes = {
   selectProps: PropTypes.object
+}
+
+export const customRenderOptions = [
+  { value: '1', label: 'app' },
+  { value: '2', label: 'bot' },
+  { value: '9', label: 'hide' },
+  { value: '10', label: 'link' },
+  { value: '3', label: 'adjust' },
+  { value: '4', label: 'archive' },
+  { value: '5', label: 'cashtag' },
+  { value: '6', label: 'emoticon' },
+  { value: '7', label: 'following' },
+  { value: '8', label: 'flags' }
+];
+
+export const IconPicker = (props: CustomRenderProps<DefaultOptions>) => {
+  return (
+    <div>
+      {props.data.label}
+      <Icon className="tk-pl-1" iconName={props.data.label} />
+    </div>
+  );
+};
+
+IconPicker.propTypes = {
+  data: PropTypes.object
 }
