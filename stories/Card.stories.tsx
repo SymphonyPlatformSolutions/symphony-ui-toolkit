c457cba113038ba42dfbfca65c34dfc27d0f9178
Anna Llorens <anna.llorens@symphony.com>
2021-01-19 15:34:06 +0100

    APP-3689 Add card component

---
+++ stories/Card.stories.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { Button, Card, Typography } from '../src/components';
+import Scale from '../src/core/hoc/Scale';
+
+const Template = (args) => {
+  return <Card {...args}></Card>;
+};
+
+export const Default = Template.bind({});
+
+Default.args = {
+  children:'Card title',
+  className:'tk-p-1'
+};
+
+const cardContent: React.ReactNode =  
+  <div className="tk-m-1">  
+    <Typography variant="bold">Card title</Typography>
+    <div className="tk-pb-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+    <Scale size="x-small">
+      <Button>Button</Button>
+    </Scale>
+  </div>;
+
+export const Content = Template.bind({});
+Content.args = {
+  children: cardContent,
+};
+
+export default {
+  title: 'Components/Card',
+};
