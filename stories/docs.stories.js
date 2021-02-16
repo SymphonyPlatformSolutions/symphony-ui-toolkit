3c9a8767a274c5638c5a9c900716149b6233ba1b
Anna Llorens <anna.llorens@symphony.com>
2021-02-16 20:06:43 +0100

    Added contributing.md to Storybook

--- stories/docs.stories.js
+++ stories/docs.stories.js
@@ -1,9 +1,11 @@
 import start from '../docs/getting-started.md'
 import theming from '../docs/theming-guide.md'
+import contributing from '../docs/contributing.md'
 
 export default {
-  title: 'Welcome/Getting Started',
+  title: 'Welcome/Docs',
 };
 
 export const GettingStarted = () => start;
 export const ThemingGuide = () => theming;
+export const Contributing = () => contributing;
