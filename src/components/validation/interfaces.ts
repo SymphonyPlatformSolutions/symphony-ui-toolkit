620bd39b53ebb452569a5cdb4b6b07ace59d1213
Jean-Michael Legait <jean-michael.legait@symphony.com>
2021-03-11 09:25:25 +0100

    APP-3928 Type additional meta-data sent in the event

--- src/components/validation/interfaces.ts
+++ src/components/validation/interfaces.ts
@@ -4,16 +4,16 @@ export enum ACTION {
   ON_CHANGE = 'onChange',
 }
 
-export type EventWithValue<T> = {
-  target: { value: T } & Record<string, unknown>;
+export type EventWithValue<T, U = Record<string, unknown>> = {
+  target: { value: T } & U;
 };
 
 export type ErrorMessages = string | { [key: string]: string };
 
 // Every component that want to be compatible with Validation Component should use this HasValidationProps props
-export type HasValidationProps<T> = {
-  onChange?: (event: EventWithValue<T>) => any;
-  onBlur?: (event: EventWithValue<T>) => any;
+export type HasValidationProps<T, U = Record<string, unknown>> = {
+  onChange?: (event: EventWithValue<T, U>) => any;
+  onBlur?: (event: EventWithValue<T, U>) => any;
   onInit?: (value: T) => any;
   onValidationChanged?: (errors: ErrorMessages) => any;
 };
