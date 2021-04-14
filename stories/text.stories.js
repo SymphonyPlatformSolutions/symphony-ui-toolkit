eb3ac6c675e19d7dd811d951207253d2d034dc9c
Selena <54842163+selena-mallet@users.noreply.github.com>
2021-04-14 14:32:36 +0200

    EIS-2792 - required label style (#77)
    add a style tk-label--required for labels

--- stories/text.stories.js
+++ stories/text.stories.js
@@ -4,4 +4,5 @@ export default {
 
 export const Text = () => `<label class="tk-label">Default label</label>
 <label class="tk-label tk-label--error">Error label</label>
+<label class="tk-label tk-label--required">Required label</label>
 <label class="tk-label tk-label--disabled">Disabled label</label>`;
