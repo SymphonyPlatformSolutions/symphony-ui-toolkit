030b51d45aa73dd875d4b8fd0a5d8007c18a3a6e
Matthieu Crouzet <matthieu.crouzet@symphony.com>
2021-04-21 17:44:23 +0200

    feat(input): forward input ref

--- src/components/input/TextArea.tsx
+++ src/components/input/TextArea.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
-import { TextComponentPropTypes, TextComponent, TextComponentProps, Types } from './TextComponent';
+import {
+  TextComponentPropTypes,
+  TextComponent,
+  TextComponentProps,
+  Types,
+} from './TextComponent';
 
-const TextArea:React.FC<TextComponentProps> = (props) => {
-  return <TextComponent type={Types.TEXTAREA} {...props} />;
-};
+const TextArea: React.FC<
+  TextComponentProps &
+    React.RefAttributes<HTMLInputElement | HTMLTextAreaElement>
+> = React.forwardRef((props, ref) => {
+  return <TextComponent type={Types.TEXTAREA} {...props} ref={ref} />;
+});
 
 TextArea.propTypes = TextComponentPropTypes;
+TextArea.displayName = 'TextArea';
 
 export default TextArea;
--- src/components/input/TextField.tsx
+++ src/components/input/TextField.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
-import { TextComponentPropTypes, TextComponentProps, TextComponent, Types } from './TextComponent';
+import {
+  TextComponentPropTypes,
+  TextComponentProps,
+  TextComponent,
+  Types,
+} from './TextComponent';
 
-const TextField:React.FC<TextComponentProps> = (props) => {
-  return <TextComponent type={Types.TEXTFIELD} {...props} />;
-};
+const TextField: React.FC<
+  TextComponentProps &
+    React.RefAttributes<HTMLInputElement | HTMLTextAreaElement>
+> = React.forwardRef((props, ref) => {
+  return <TextComponent type={Types.TEXTFIELD} {...props} ref={ref} />;
+});
 
 TextField.propTypes = TextComponentPropTypes;
+TextField.displayName = 'TextField';
 
 export default TextField;
