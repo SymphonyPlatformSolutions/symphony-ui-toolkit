34efc359c0c3b30f403857ad38eb868ae3897376
Anna Llorens <anna.llorens@symphony.com>
2021-05-21 10:21:49 +0200

    APP-4073 Remove scale from storybook examples

--- stories/Card.stories.tsx
+++ stories/Card.stories.tsx
@@ -17,9 +17,7 @@ const cardContent: React.ReactNode =
   <div className="tk-m-1">  
     <Typography variant="bold">Card title</Typography>
     <div className="tk-pb-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
-    <Scale size="x-small">
-      <Button>Button</Button>
-    </Scale>
+    <Button>Button</Button>
   </div>;
 
 export const Content = Template.bind({});
--- stories/Loader.stories.tsx
+++ stories/Loader.stories.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Loader from '../src/components/loader';
-import Scale from '../src/core/hoc/Scale';
 
 const Template = (args) => {
   return <Loader {...args} />;
@@ -15,26 +14,6 @@ export const Spinner: React.FC = () => (
     <Loader variant="attention" className="tk-ml-2h" />
     <Loader variant="warning" className="tk-ml-2h" />
     <Loader variant="ok" className="tk-ml-2h" />
-    <h2 className="tk-mt-4h">Sizes</h2>
-    <p>You can use the Scale component to display the loading in different sizes: </p>
-    <Scale size="x-small" className="tk-pr-2h">
-      <Loader className="tk-ml-h" />
-    </Scale>
-    <Scale size="small" className="tk-pr-2h">
-      <Loader className="tk-ml-h" />
-    </Scale>
-    <Scale size="medium" className="tk-pr-2h">
-      <Loader className="tk-ml-h" />
-    </Scale>
-    <Scale size="large" className="tk-pr-2h">
-      <Loader className="tk-ml-h" />
-    </Scale>
-    <Scale size="x-large" className="tk-pr-2h">
-      <Loader className="tk-ml-h" />
-    </Scale>
-    <Scale size="xx-large" className="tk-pr-2h">
-      <Loader className="tk-ml-h" />
-    </Scale>
   </div>
 );
 
