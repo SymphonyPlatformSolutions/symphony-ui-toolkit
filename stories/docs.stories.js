9fac9fe5cd5ff6647e6e00a985cd0eb8b4a840c7
Jean-Michael Legait <66668470+symphony-jean-michael@users.noreply.github.com>
2021-06-03 17:39:39 +0200

    APP-3149 Add CodeSandbox (#99)
    * APP-3149 Add CodeSandbox
    
    * APP-3149 Rename CodeSandbox project
    
    * APP-3149 Add Sanbox section

--- stories/docs.stories.js
+++ stories/docs.stories.js
@@ -1,6 +1,7 @@
-import start from '../docs/getting-started.md'
-import theming from '../docs/theming-guide.md'
-import contributing from '../docs/contributing.md'
+import start from '../docs/getting-started.md';
+import theming from '../docs/theming-guide.md';
+import contributing from '../docs/contributing.md';
+import sandbox from '../docs/sandbox.md';
 
 export default {
   title: 'Welcome/Docs',
@@ -9,3 +10,4 @@ export default {
 export const GettingStarted = () => start;
 export const ThemingGuide = () => theming;
 export const Contributing = () => contributing;
+export const Sandbox = () => sandbox;
