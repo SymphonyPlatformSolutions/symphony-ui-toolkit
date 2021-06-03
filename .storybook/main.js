a83087878c97c44fb1103c2eece929546e9974be
Jean-Michael Legait <jean-michael.legait@symphony.com>
2021-06-03 11:06:14 +0200

    APP-3149 Add MDX-Embed add-on

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
     '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-a11y',
+    'storybook-addon-mdx-embed',
     'storybook-dark-mode/register',
     '@storybook/addon-knobs/register', // Keep until we migrate all to addon-controls
     '@storybook/addon-controls',
