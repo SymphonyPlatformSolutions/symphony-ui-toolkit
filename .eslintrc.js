9ebac808575e7e34e6be724848171636509aa532
Jean-Michael Legait <jean-michael.legait@symphony.com>
2021-06-03 11:05:46 +0200

    APP-3149 Fix escape-quote incompatibility between ESLint and Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,27 +3,27 @@ module.exports = {
   env: {
     browser: true,
     jest: true,
-    commonjs: true
+    commonjs: true,
   },
   settings: {
     react: {
-      version: 'detect'
-    }
+      version: 'detect',
+    },
   },
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended'
+    'plugin:react/recommended',
   ],
   rules: {
     'react/jsx-props-no-spreading': ['off'],
-    quotes: ['error', 'single'],
+    quotes: ['error', 'single', 'avoid-escape'],
     'jsx-quotes': ['error', 'prefer-double'],
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     indent: ['error', 2],
     '@typescript-eslint/no-var-requires': 0,
-  }
+  },
 };
