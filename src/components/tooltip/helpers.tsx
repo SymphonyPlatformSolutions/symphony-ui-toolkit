b7f72ae99e577ff617495fde8ca9a877bee6a08e
Matthieu Crouzet <77045492+matthieucrouzet-sym@users.noreply.github.com>
2021-06-14 10:23:38 +0200

    fix: tooltip behaviour (#235)

--- src/components/tooltip/helpers.tsx
+++ src/components/tooltip/helpers.tsx
@@ -1,45 +1,50 @@
-import * as React from 'react'
+import * as React from 'react';
 
-export const showTooltipOnClick = (children: React.ReactNode, showClick: boolean, setShowClick: React.Dispatch<React.SetStateAction<boolean>>) => {
-  return React.Children.map(
-    children, child => {
-      if (React.isValidElement(child)) {
-        return React.cloneElement(child, {
-          onClick: (event) => {
-            setShowClick(!showClick);
-            if (child.props.onClick) {
-              child.props.onClick(event);
-            }
+export const showTooltipOnClick = (
+  children: React.ReactNode,
+  showClick: boolean,
+  setShowClick: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+  return React.Children.map(children, (child) => {
+    if (React.isValidElement(child)) {
+      return React.cloneElement(child, {
+        onClick: (event) => {
+          setShowClick(!showClick);
+          if (child.props.onClick) {
+            child.props.onClick(event);
           }
-        })
-      } else {
-        return <span onClick={ () => setShowClick(!showClick)}> { child } </span>
-      }
+        },
+      });
+    } else {
+      return <span onClick={() => setShowClick(!showClick)}> {child} </span>;
     }
-  )
-}
+  });
+};
 
-export const showTooltipOnHover = (children: React.ReactNode, setShowHover: React.Dispatch<React.SetStateAction<boolean>>) => {
-  return React.Children.map(
-    children, child => {
-      if (React.isValidElement(child)) {
-        return React.cloneElement(child, {
-          onMouseEnter: (event) => {
-            setShowHover(true)
-            if (child.props.onMouseEnter) {
-              child.props.onMouseEnter(event);
-            }
-          },
-          onMouseLeave: (event) => {
-            setShowHover(false)
-            if (child.props.onMouseLeave) {
-              child.props.onMouseLeave(event);
-            }
+export const showTooltipOnHover = (
+  children: React.ReactNode,
+  setShowHover: (showHover: boolean) => void
+) => {
+  return React.Children.map(children, (child) => {
+    if (React.isValidElement(child)) {
+      return React.cloneElement(child, {
+        onMouseEnter: (event) => {
+          setShowHover(true);
+          if (child.props.onMouseEnter) {
+            child.props.onMouseEnter(event);
           }
-        })
-      } else {
-        return <span onMouseEnter={ () => setShowHover(true)} onMouseLeave={ () => setShowHover(false)} > { child } </span>
-      }
+        },
+        onMouseLeave: (event) => {
+          setShowHover(false);
+          if (child.props.onMouseLeave) {
+            child.props.onMouseLeave(event);
+          }
+        },
+      });
+    } else {
+      return (
+        <span onMouseEnter={() => setShowHover(true)} onMouseLeave={() => setShowHover(false)}>{child}</span>
+      );
     }
-  )
-}
+  });
+};
