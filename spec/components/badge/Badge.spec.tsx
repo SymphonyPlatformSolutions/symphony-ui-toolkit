929ed5240241bd11c6edefff3cc7c5d413c68e14
Nolan Potier <68021464+nolan-symphony@users.noreply.github.com>
2021-07-16 09:49:33 +0200

    SYM-4424: Badge component (#258)
    * feat: badge
    
    * fix: code review
    
    Co-authored-by: Jean-Michael Legait <66668470+symphony-jean-michael@users.noreply.github.com>

---
+++ spec/components/badge/Badge.spec.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+
+import Badge from '../../../src/components/badge/Badge';
+
+describe('Badge Component', () => {
+  it('render with default props does not crash', () => {
+    const wrapper = shallow(<Badge>Text</Badge>);
+    expect(wrapper.length).toEqual(1);
+    expect(wrapper.hasClass('tk-badge')).toBe(true);
+  });
+  it('render a variant', () => {
+    const wrapper = shallow(<Badge variant="attention">Badge</Badge>);
+    expect(wrapper.length).toEqual(1);
+    expect(wrapper.find('div').hasClass('tk-badge--attention')).toBe(true);
+  });
+});
---
+++ src/components/badge/Badge.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+const prefix = 'tk-badge';
+
+export interface BadgeProps extends React.HTMLProps<HTMLDivElement>  {
+  /** Content of the badge */
+  children?: React.ReactNode;
+  className?: string;
+  /** The variant to use */
+  variant?: 'positive' | 'neutral' | 'attention' | 'warning' | 'external';
+}
+
+const Badge: React.FC<BadgeProps> = ({
+  children,
+  className,
+  variant,
+  ...rest
+}) => {
+  const classes = classNames(
+    className,
+    prefix,
+    `${prefix}--${variant}`,
+  );
+  return (
+    <div
+      className={classes}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+};
+
+
+Badge.propTypes = {
+  children: PropTypes.any,
+  className: PropTypes.string,
+  variant: PropTypes.oneOf(['positive', 'neutral', 'attention', 'warning', 'external']),
+}
+Badge.displayName = 'Badge';
+export default Badge;
