397fb2f6793cbc66a9eac958673006927e1ba100
Nolan Potier <nolan.potier@symphony.com>
2021-07-26 08:50:16 +0200

    add all combination

--- stories/Badge.stories.tsx
+++ stories/Badge.stories.tsx
@@ -63,8 +63,55 @@ export const EXTernal: React.FC = () => (
 
 export const BadgeSizes: React.FC = () => (
   <>
-    <Badge size="medium" className="tk-mr-h">Badge</Badge>
-    <Badge size="small" className="tk-mr-h">Badge</Badge>
+    <div>
+      <Badge size="medium" className="tk-mr-h">Badge</Badge>
+      <Badge size="small" className="tk-mr-h">Badge</Badge>
+      <Badge size="small">
+        <Icon iconName="bot" className="tk-mr-h"></Icon>
+        Badge
+      </Badge>
+    </div>
+    <div className="tk-mt-h">
+      <Badge variant="positive" size="medium" className="tk-mr-h">Badge</Badge>
+      <Badge variant="positive" size="small" className="tk-mr-h">Badge</Badge>
+      <Badge size="small" variant="positive">
+        <Icon iconName="announce" className="tk-mr-h"></Icon>
+        Badge
+      </Badge>
+    </div>
+    <div className="tk-mt-h">
+      <Badge variant="neutral" size="medium" className="tk-mr-h">Badge</Badge>
+      <Badge variant="neutral" size="small" className="tk-mr-h">Badge</Badge>
+      <Badge size="small" variant="neutral">
+        <Icon iconName="announce" className="tk-mr-h"></Icon>
+        Badge
+      </Badge>
+    </div>
+    <div className="tk-mt-h">
+      <Badge variant="attention" size="medium" className="tk-mr-h">Badge</Badge>
+      <Badge variant="attention" size="small" className="tk-mr-h">Badge</Badge>
+      <Badge size="small" variant="attention">
+        <Icon iconName="alert-round" className="tk-mr-h"></Icon>
+        Badge
+      </Badge>
+    </div>
+    <div className="tk-mt-h">
+      <Badge variant="warning" size="medium" className="tk-mr-h">Badge</Badge>
+      <Badge variant="warning" size="small" className="tk-mr-h">Badge</Badge>
+      <Badge size="small" variant="warning">
+        <Icon iconName="alert-triangle" className="tk-mr-h"></Icon>
+        Badge
+      </Badge>
+    </div>
+    <div className="tk-mt-h">
+      <Badge variant="external" size="medium" className="tk-mr-h">Badge</Badge>
+      <Badge variant="external" size="small" className="tk-mr-h">Badge</Badge>
+      <Badge variant="external" size="small">
+        <Icon iconName="bot" className="tk-mr-h"></Icon>
+        Badge
+      </Badge>
+    </div>
+
   </>);
 
 export default {
