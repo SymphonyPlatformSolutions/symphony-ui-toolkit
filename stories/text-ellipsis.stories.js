a86f1786d5f72e9bfc28757907d623ca7c8440bd
Axel Eriksson <60875212+axeleriksson147@users.noreply.github.com>
2021-07-27 15:23:12 +0200

    APP-3334 - Adding text-ellipsis (#120)
    * Adding text-ellipsis
    
    * Update storybook
    
    * Text-ellipsis
    
    * Cleanup
    
    * text-ellipsiss

---
+++ stories/text-ellipsis.stories.js
@@ -0,0 +1,27 @@
+export default {
+    title: 'Components/Text Ellipsis',
+};
+
+export const TextEllipsis = () => {
+    return (`
+    <div style="min-height: 600px">
+    
+        <h1>Text Ellipsis</h1>
+        
+        <li>1 row</li>
+        <div style="background: grey; margin: 16px 0px; padding: 16px; width: 200px">
+            <p class="tk-text-ellipsis" style="-webkit-line-clamp: 1">
+                Really, really, really, really, really, really, long text that gets cut!
+            </p>
+        </div>
+
+        <li>2 rows</li>
+        <div style="background: grey; margin: 16px 0px; padding: 16px; width: 200px">
+            <p class="tk-text-ellipsis tk-text-ellipsis__multiple-rows" style="-webkit-line-clamp: 2">
+                Really, really, really, really, really, really, long text that gets cut!
+            </p>
+        </div>
+    
+    </div>
+    `);
+};
