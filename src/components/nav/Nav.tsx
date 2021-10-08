a0682affc269acadbbae7594e32d976c7ad69605
antoinerollindev <66251236+antoinerollindev@users.noreply.github.com>
2021-10-08 10:46:12 +0200

    APP-4572: Fix `Nav` not updating on `activeItemId` prop change (#305)
    - converts `Nav` from class component to function component
    - preious point allows to use `useEffect` in order to watch a prop change `activeItemId` (which is a fix compared to the previous implementation)
    - push `Nav` test coverage to 100%

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,60 +1,60 @@
-
 import * as React from 'react';
+import classnames from 'classnames';
 
 export type NavProps = {
-  /** List of items displayed on the Navigation bar */
+  /** List of items displayed in the navigation bar */
   items: NavItem[];
-  /** If is not defined, the first item will be active by default. */
-  activeItemId?: number|string;
+  /** Active item */
+  activeItemId?: number | string;
+  /** Function called when an item is clicked */
   onActiveTabChange?: (navItem: NavItem) => void;
-};
-type NavState = {
-  activeNavItemId;
 };
 
 export interface NavItem {
   label: string;
-  id: number|string;
-  
+  id: number | string;
+
   // Not supported so far keep until supported
   // isDisabled?: boolean;
 }
 
-export class Nav extends React.Component<NavProps, NavState> {
+export const Nav: React.FC<NavProps> = ({
+  items,
+  activeItemId,
+  onActiveTabChange,
+}: NavProps) => {
 
-  state = {
-    activeNavItemId: this.props?.activeItemId || this.props.items[0]?.id,
-  }
+  const [activeNavItemId, setActiveNavItemId] = React.useState(items?.[0]?.id);
 
-  onClick = (e: React.MouseEvent<HTMLElement>, navItem: NavItem) => {
-    this.setState({ activeNavItemId: navItem.id});
-    if(this.props.onActiveTabChange) {
-      this.props.onActiveTabChange(navItem);
+  React.useEffect(() => {
+    if (activeItemId) {
+      setActiveNavItemId(activeItemId);
     }
-  };
+  }, [activeItemId]);
 
-  render() {
-    const {items} = this.props;
-    const {activeNavItemId} = this.state;
-
-    return (
-      <div>
-        <nav className="tk-nav">
-          <ul className="tk-nav--tabs">
-            {items.map((item) => (
-              <li
-                key={item.id}
-                onClick={(e) => this.onClick(e, item)}
-                className={`tk-nav-item ${activeNavItemId === item.id? 'tk-nav-item--active':''}`}
-              >
-                {item.label}
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
-    );
+  const handleClick = (item: NavItem) => {
+    setActiveNavItemId(item.id);
+    if (onActiveTabChange) {
+      onActiveTabChange(item);
+    }
   }
-}
+
+  return (
+    <div>
+      <nav className="tk-nav">
+        <ul className="tk-nav--tabs">
+          {items.map((item) => (
+            <li
+              key={item.id}
+              onClick={() => handleClick(item)}
+              className={classnames('tk-nav-item', { 'tk-nav-item--active': activeNavItemId === item.id })}>
+              {item.label}
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  )
+};
 
 export default Nav;
