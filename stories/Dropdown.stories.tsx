170d7857a613189018bf4114e36d85aaaaf4509d
Jean-Michael Legait <jean-michael.legait@symphony.com>
2021-10-12 09:17:47 +0200

    APP-4556 Ad Portal with Dropdown Example

--- stories/Dropdown.stories.tsx
+++ stories/Dropdown.stories.tsx
@@ -242,7 +242,28 @@ export const Select: React.FC = () => (
       options={defaultOptions}
       menuShouldScrollIntoView={false}
     />
+
+    <h2 className="tk-mt-5h">Menu in a Portal</h2>
+    <p>{'We expose a menuPortalTarget prop, that lets you portal the dropdown menu to a dom node of your choosing.'}</p>
+    <ul>
+      <li><strong>menuPortalTarget</strong>: Whether the menu should use a portal, and where it should attach.
+        <p>In this example, we attached the Menu to the document.body</p>
+      </li>
+      <li><strong>menuShouldBlockScroll</strong>: Whether to block scroll events when the menu is open.
+        <p>ℹ️ Displaying the menu in a portal causes the menu to stay fixed in place and not scroll with the parent container.Therefore this can appear as a bug, to solve this behaviour we advise you to block the scroll when the menu is open.</p>
+      </li>
+      <li><strong>menuPortalStyles</strong>: Custom styles applied on menu portal.
+        <p>ℹ️ The menu is displayed by using some JSS (CSS in JS), but you can overwrite the inline CSS properties by using this attribute.</p>
+      </li>
+    </ul>
+    <Dropdown
+      options={defaultOptions}
+      menuPortalTarget={document.body}
+      menuShouldBlockScroll={true}
+      menuPortalStyles={{ zIndex: 100 }}
+    />
   </div>
+
 );
 
 export default {
