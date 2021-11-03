b96acf2e42f185916b8ad5e99f4f5fd1ae114f23
Anna Llorens <68587873+symphony-annaLlorens@users.noreply.github.com>
2021-11-03 14:28:33 +0100

    APP-4594 Added maxLength validator (#322)
    * APP-4594 Added maxLength validator
    
    * APP-4594 PR review
    
    * APP-4595 Pr review
    
    * APP-4594 Updated ut
    
    * APP-4594 Update validator to accept array
    
    * APP-4594 Updated ut
    
    * APP-4594 Remove duplicated block code

--- spec/core/validators/validators.spec.tsx
+++ spec/core/validators/validators.spec.tsx
@@ -6,6 +6,8 @@ describe('Input Validators', () => {
     expect(await Validators.Required('')).toEqual({ required: true });
     expect(await Validators.Required('  ')).toEqual({ required: true });
     expect(await Validators.Required('a')).toEqual(null);
+    expect(await Validators.Required([])).toEqual({ required: true });
+    expect(await Validators.Required(undefined)).toEqual({ required: true });
   });
   it('should validate required field correcltly', async () => {
     expect(await Validators.Email(null)).toEqual(null);
@@ -23,11 +25,25 @@ describe('Input Validators', () => {
     expect(await Validators.EmptyString('')).toEqual({ emptyString: true });
     expect(await Validators.EmptyString(' ')).toEqual({ emptyString: true });
     expect(await Validators.EmptyString('a')).toEqual(null);
+    expect(await Validators.EmptyString([])).toEqual({ emptyString: true });
+    expect(await Validators.EmptyString([{value:'a'}])).toEqual(null);
+    expect(await Validators.EmptyString(undefined)).toEqual({ emptyString: true });
   });
 
   it('should validate minlength correctly', async () => {
     expect(await Validators.MinLength(3)('a')).toEqual({ minlength: true });
     expect(await Validators.MinLength(3)('abcd')).toEqual(null);
+    expect(await Validators.MinLength(3)([])).toEqual({ minlength: true });
+    expect(await Validators.MinLength(2)([{value:'a'}])).toEqual( {minlength: true});
+    expect(await Validators.MinLength(3)(undefined)).toEqual({ minlength: true });
+  });
+  it('should validate maxLength correctly', async () => {
+    expect(await Validators.MaxLength(3)('a')).toEqual(null);
+    expect(await Validators.MaxLength(3)('')).toEqual(null);
+    expect(await Validators.MaxLength(3)([])).toEqual(null);
+    expect(await Validators.MaxLength(1)([{value:'a'},{value:'b'}])).toEqual( {maxLength: true});
+    expect(await Validators.MaxLength(3)(undefined)).toEqual(null);
+    expect(await Validators.MaxLength(3)('abcd')).toEqual({ maxLength: true });
   });
   it('should validate number correctly', async () => {
     expect(await Validators.Number('a')).toEqual({ number: true });
--- stories/Validation.stories.tsx
+++ stories/Validation.stories.tsx
@@ -102,6 +102,18 @@ export const Validations = () => {
         <TextField placeholder="How are you?" />
       </Validation>
       <p>
+        with <strong>maxLength validator</strong>
+      </p>
+      <Validation
+        onValidationChanged={logChange}
+        validator={
+          Validators.MaxLength(6)
+        }
+        errorMessage={ 'Booking code: max 6 characters'}
+      >
+        <TextField label="Booking code" placeholder="Booking code" />
+      </Validation>
+      <p>
         Using <strong>pattern validator</strong>
       </p>
       <Validation
@@ -170,6 +182,31 @@ export const Validations = () => {
             setMultiDropdown(e.target.value);
           }}
           isMultiSelect
+        />
+      </Validation>
+      <p>
+        with <strong>maxLength validator</strong>
+      </p>
+      <Validation
+        onValidationChanged={logChange}
+        validator={[
+          Validators.MaxLength(6),
+        ]}
+        errorMessage={ 'Select max 6 options'}
+      >
+        <Dropdown
+          isMultiSelect
+          label={'Dropdown label'}
+          placeHolder="Multi select..."
+          options={[
+            { value: '1', label: 'option 1' },
+            { value: '2', label: 'option 2' },
+            { value: '3', label: 'option 3' },
+            { value: '4', label: 'option 4' },
+            { value: '5', label: 'option 5' },
+            { value: '6', label: 'option 6' },
+            { value: '7', label: 'option 7' },
+          ]}    
         />
       </Validation>
       <h3>InputDecorator</h3>
@@ -270,7 +307,7 @@ export const Validations = () => {
       <h2>Multiple validators</h2>
       <p>
         Text Field with <strong>multiple validators</strong>: Mandatory number,
-        with a minimum length of 3 characters
+        with a minimum length of 3 characters, max length of 4
       </p>
       <Validation
         onValidationChanged={logChange}
@@ -278,11 +315,13 @@ export const Validations = () => {
           Validators.Required,
           Validators.Number,
           Validators.MinLength(3),
+          Validators.MaxLength(4),
         ]}
         errorMessage={{
           required: 'This field is mandatory',
           number: 'Should be a number',
           minlength: 'Please type at least 3 numbers',
+          maxLength: 'Max 4 characters',
         }}
       >
         <TextField label="Number" placeholder="Age" />
