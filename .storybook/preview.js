d4d9f74db2afeb750a312f306513eae88e5bfc43
AliciaSymphony <alicia.marin@symphony.com>
2021-11-04 14:11:48 +0100

    Add addon-themes for condensed more

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -14,7 +14,12 @@ addParameters({
     // Override the default dark theme
     dark: { ...themes.dark, appContentBg: '#1A1C1D',  brandImage: 'https://theme.zdassets.com/theme_assets/643007/9ede1116ae6efc6e4fb12ba49e6315c803807d56.png', brandTitle: 'UIToolkit Components', },
     light: { ...themes.light, appBg: 'white', brandImage: 'https://symphony.com/wp-content/uploads/2019/06/logo.png', brandTitle: 'UIToolkit Components',}
-  }
+  },
+  themes: {
+    list: [
+      { name: 'Condensed', class: ['tk-theme-condensed', 'condensed']}
+    ],
+  },
 });
 // this allows us to add a dark class to body element to be able to render our component in dark mode  in sync with storybook's
 const init = () => {
