ae8a053e410cc45ec52c18be67662442c4a04ba0
Jean-Michael Legait <66668470+symphony-jean-michael@users.noreply.github.com>
2021-12-06 12:11:43 +0100

    APP-4662 Remove Tempo-UI files from UI-Toolkit (#182)

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-yarn sass --load-path=node_modules --no-source-map --style compressed uitoolkit.scss:dist/css/uitoolkit.css uitoolkit-tempo-support.scss:dist/css/uitoolkit-tempo-support.css uitoolkit-cdn.generated.scss:dist/css/uitoolkit-cdn.generated.css
+yarn sass --load-path=node_modules --no-source-map --style compressed uitoolkit.scss:dist/css/uitoolkit.css uitoolkit-cdn.generated.scss:dist/css/uitoolkit-cdn.generated.css
 AUTOPREFIXER_GRID=autoplace yarn postcss ./dist/css/*.css --use autoprefixer -d ./dist/css --no-map
