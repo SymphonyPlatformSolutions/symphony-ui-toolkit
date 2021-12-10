73d0a43627b229430dba8077f32fb9d2a603b624
Anna Llorens <anna.llorens@symphony.com>
2021-12-10 12:18:11 +0100

    APP-4692 Export readme to storybook

--- stories/docs.stories.js
+++ stories/docs.stories.js
@@ -1,4 +1,5 @@
 import start from '../docs/getting-started.md';
+import readme from '../README.md';
 import theming from '../docs/theming-guide.md';
 import contributing from '../docs/contributing.md';
 import sandbox from '../docs/sandbox.md';
@@ -14,6 +15,7 @@ export default {
   },
 };
 
+export const Readme = () => readme;
 export const GettingStarted = () => start;
 export const ThemingGuide = () => theming;
 export const Contributing = () => contributing;
