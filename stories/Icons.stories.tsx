699ba9f8b2dedbb02ac19be035fa9df7a31f3059
Anna Llorens <anna.llorens@symphony.com>
2021-12-21 12:08:00 +0100

    APP-4647 Refactor icon stories

--- stories/Icons.stories.tsx
+++ stories/Icons.stories.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
 import Icon from '../src/components/icon/Icon';
-import NameOfIcons from '@symphony-ui/uitoolkit-styles/src/icons/tk-icons.codepoints.json';
+import IconCodepoints from '@symphony-ui/uitoolkit-styles/src/icons/tk-icons.codepoints.json';
 import { TkIcon } from '@symphony-ui/uitoolkit-styles/dist/fonts/tk-icons';
 
-const takeName = Object.keys(NameOfIcons);
-const sortName = takeName.sort();
-
+const iconNames  = Object.keys(IconCodepoints).sort();
 export const Icons: React.FC = () => (
   <div>
-    {sortName.map((value, index) => {
+    {iconNames.map((value, index) => {
       return (
         <div key={index} className="preview">
           <Icon iconName={value as TkIcon}></Icon>
