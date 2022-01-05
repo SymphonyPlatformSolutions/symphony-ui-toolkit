12d0c8da4f81f02d5d7823f5e9e6813d8b5c5497
Jean-Michael Legait <jean-michael.legait@symphony.com>
2022-01-05 15:30:18 +0100

    ♻ Cleaning repo after migrating on monorepo

---
+++ setVersion.sh
@@ -0,0 +1,19 @@
+
+# Set version
+if [ -z "$CIRCLE_TAG" ]; then
+    echo "No tag, skip publish ..."
+else
+    echo "Found tag, setting version to $CIRCLE_TAG"
+    yarn version --no-git-tag-version --new-version ${CIRCLE_TAG//v}
+    yarn workspace @symphony-ui/uitoolkit-styles version --no-git-tag-version --new-version ${CIRCLE_TAG//v}
+    yarn workspace @symphony-ui/uitoolkit-components version --no-git-tag-version --new-version ${CIRCLE_TAG//v}
+    # Update Components with new release of Styles
+    yarn workspace @symphony-ui/uitoolkit-components upgrade @symphony-ui/uitoolkit-styles@${CIRCLE_TAG//v}
+
+    git add package.json
+    git add **/package.json
+    git config --global user.email "$GIT_USEREMAIL"
+    git config --global user.name "$GIT_USERNAME"
+    git commit -m "[skip ci] AUTO Bump version"
+    git push origin HEAD:master
+fi
