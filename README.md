3593727f45a5884db1769161055baebd506eb168
Jean-Michael Legait <jean-michael.legait@symphony.com>
2022-01-06 15:17:31 +0100

    ♻ Cleaning and updating repo after migrating on monorepo

---
+++ README.md
@@ -0,0 +1,6 @@
+# Symphony - UI Toolkit
+
+## Packages
+
+See: [UI Toolkit Components: The React components library of Symphony`s design system](packages/components/README.md)
+See: [UI Toolkit Styles: The official styles library of Symphony's design system](packages/styles/README.md)
--- packages/components/publish.sh
+++ packages/components/publish.sh
@@ -5,28 +5,29 @@ function executePublish {
     cp package.json dist/
     cp README.md dist/
     cd dist
-    yarn publish --access public
+#    yarn publish --access public #Temporaty disable publish
+    echo "[MOCK COMPONENTS] yarn publish"
     cd ..
-    git add package.json
-    git config --global user.email "$GIT_USEREMAIL"
-    git config --global user.name "$GIT_USERNAME"
-    git commit -m "[skip ci] AUTO Bump version"
-    git push origin HEAD:master
+    #git add package.json
+    #git config --global user.email "$GIT_USEREMAIL"
+    #git config --global user.name "$GIT_USERNAME"
+    #git commit -m "[skip ci] AUTO Bump version"
+    #git push origin HEAD:master
 }
 
 # Set version
-function setVersion {
-    echo "Found tag, setting version to $CIRCLE_TAG"
-    yarn version --no-git-tag-version --new-version ${CIRCLE_TAG//v}
-}
+#function setVersion {
+#    echo "Found tag, setting version to $CIRCLE_TAG"
+#    yarn version --no-git-tag-version --new-version ${CIRCLE_TAG//v}
+#}
 
 # Version and publish logic
 function publish() {
-    echo "Running publish..."
-    setVersion;
+    echo "Running UI-Toolkit Components publish..."
+#    setVersion;
     executePublish;
-    postToUniversalWebhook;
-    echo "Publish End"
+#    postToUniversalWebhook; # Temporary disable notification
+    echo "Publish UI-Toolkit Components End"
 }
 
 function postToUniversalWebhook() {
