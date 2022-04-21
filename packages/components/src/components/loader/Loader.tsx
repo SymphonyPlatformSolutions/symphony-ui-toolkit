03c2451dfabbc046aa90adff06fad3fc475d7bea
Symphony-cyril <cyril.ruffa@symphony.com>
2022-04-21 16:09:23 +0200

    add loading text to loader component

--- packages/components/src/components/loader/Loader.tsx
+++ packages/components/src/components/loader/Loader.tsx
@@ -10,35 +10,44 @@ type LoaderProps = {
   type?: 'spinner';
   /** The variant to use */
   variant?: 'primary' | 'attention' | 'warning' | 'ok';
+  loadingText?: string;
+  loadingTextPos?: 'bottom' | 'right';
 };
 
 const Loader: React.FC<LoaderProps> = ({
   className,
   variant,
   type,
+  loadingText,
+  loadingTextPos,
   ...rest
 }: LoaderProps) => {
-  const classes = classNames(
-    className,
-    `${prefix}-${type}`,
-    {[`${prefix}--${variant}`]: variant},
-  );
-  return (
-    <i
-      className={classes}
-      {...rest}
-    >
-    </i>
+  const classes = classNames(className, `${prefix}-${type}`, {
+    [`${prefix}--${variant}`]: variant,
+  });
+  const textClasses = classNames(className, {
+    [`${prefix}-textPos--${loadingTextPos}`]: loadingTextPos,
+  });
+  return loadingText ? (
+    <div className={textClasses}>
+      <i className={classes} {...rest}></i>
+      <p className="tk-loader-text">{loadingText}</p>
+    </div>
+  ) : (
+    <i className={classes} {...rest}></i>
   );
 };
 
 Loader.defaultProps = {
   type: 'spinner',
+  loadingTextPos: 'bottom',
 };
 
 Loader.propTypes = {
   className: PropTypes.string,
   type: PropTypes.oneOf(['spinner']),
   variant: PropTypes.oneOf(['primary', 'attention', 'warning', 'ok']),
-}
+  loadingText: PropTypes.string,
+  loadingTextPos: PropTypes.oneOf(['bottom', 'right']),
+};
 export default Loader;
