f8d451e3299b5121d50a0f3b6e7a2c84fa2aab2e
Symphony-cyril <cyril.ruffa@symphony.com>
2022-04-25 15:40:24 +0200

    Update unit test of loader

--- packages/components/spec/components/loader/Loader.spec.tsx
+++ packages/components/spec/components/loader/Loader.spec.tsx
@@ -5,12 +5,18 @@ import Loader from '../../../src/components/loader';
 describe('Loader Component', () => {
   describe('Loader component test suite => ', () => {
     it('should render the component with default props', () => {
-      const wrapper = shallow(<Loader/>);
+      const wrapper = shallow(<Loader />);
       expect(wrapper.hasClass('tk-loader-spinner')).toBe(true);
     });
     it('should render extra props to the loader component', () => {
-      const wrapper = shallow(<Loader variant="ok">Close me</Loader>);
-      expect(wrapper.hasClass('tk-loader-spinner tk-loader--ok')).toBe(true);
+      const wrapper = shallow(
+        <Loader variant="ok" size="medium">
+          Close me
+        </Loader>
+      );
+      expect(
+        wrapper.hasClass('tk-loader-spinner tk-loader--ok tk-loader--medium')
+      ).toBe(true);
     });
   });
-}); 
+});
