0ae270c2e7a1baf1933fcec2adf0894233285cde
Symphony-cyril <cyril.ruffa@symphony.com>
2022-04-28 14:00:42 +0200

    Add style to LoaderBeta.stories

--- packages/styles/stories/loader.stories.js
+++ packages/styles/stories/loader.stories.js
@@ -3,25 +3,48 @@ export default {
 };
 
 export const Spinner = () =>
-`
+  `
 <div class="tk-ml-2">
 <div class="tk-ml-2">
   <h3>Default</h3>
   <i class="tk-loader-spinner tk-loader--default"></i>
   <h3 class="tk-mt-5">Variants</h3>
-
-  <i class="tk-loader-spinner tk-loader--primary"></i>
+  <i class="tk-loader-spinner tk-loader--default"></i>
+  <i class="tk-loader-spinner tk-loader--primary tk-ml-2h"></i>
   <i class="tk-loader-spinner tk-loader--ok tk-ml-2h"></i>
   <i class="tk-loader-spinner tk-loader--warning tk-ml-2h"></i>
   <i class="tk-loader-spinner tk-loader--attention tk-ml-2h "></i>
 
-  <h3 class="tk-mt-5">Sizes</h3>
-  <i class="tk-loader-spinner tk-size-xx-small"></i>
-  <i class="tk-loader-spinner tk-size-x-small tk-ml-2h"></i>
-  <i class="tk-loader-spinner tk-size-small tk-ml-2h"></i>
-  <i class="tk-loader-spinner tk-size-medium tk-ml-2h "></i>
-  <i class="tk-loader-spinner tk-size-large tk-ml-2h "></i>
-  <i class="tk-loader-spinner tk-size-x-large tk-ml-2h "></i>
-  <i class="tk-loader-spinner tk-size-xx-large tk-ml-2h "></i>
+  <h2 class="tk-mt-5">Sizes</h2>
+  <h4 class="tk-mt-5">Small</h4>
+  <i class="tk-loader-spinner tk-loader--small"></i>
+  <i class="tk-loader-spinner tk-loader--small tk-ml-2h tk-loader--primary tk-ml-2h"></i>
+  <i class="tk-loader-spinner tk-loader--small tk-ml-2h tk-loader--ok tk-ml-2h"></i>
+  <i class="tk-loader-spinner tk-loader--small tk-ml-2h tk-loader--warning tk-ml-2h"></i>
+  <i class="tk-loader-spinner tk-loader--small tk-ml-2h tk-loader--attention tk-ml-2h"></i>
+  <h4 class="tk-mt-5">Medium</h4>
+  <i class="tk-loader-spinner tk-loader--medium"></i>
+  <i class="tk-loader-spinner tk-loader--medium tk-ml-2h tk-loader--primary tk-ml-2h"></i>
+  <i class="tk-loader-spinner tk-loader--medium tk-ml-2h tk-loader--ok tk-ml-2h"></i>
+  <i class="tk-loader-spinner tk-loader--medium tk-ml-2h tk-loader--warning tk-ml-2h"></i>
+  <i class="tk-loader-spinner tk-loader--medium tk-ml-2h tk-loader--attention tk-ml-2h"></i>
+  <h4 class="tk-mt-5">Large</h4>
+  <i class="tk-loader-spinner tk-loader--large"></i>
+  <i class="tk-loader-spinner tk-loader--large tk-ml-2h tk-loader--primary tk-ml-2h"></i>
+  <i class="tk-loader-spinner tk-loader--large tk-ml-2h tk-loader--ok tk-ml-2h"></i>
+  <i class="tk-loader-spinner tk-loader--large tk-ml-2h tk-loader--warning tk-ml-2h"></i>
+  <i class="tk-loader-spinner tk-loader--large tk-ml-2h tk-loader--attention tk-ml-2h"></i>
+
+  <h2 class="tk-mt-5">Loading Text Positions</h2>
+  <div class="tk-loader-container">
+    <div class="tk-ml-h tk-loader-textPos--bottom">
+    <i class="tk-ml-h tk-loader-spinner"></i>
+    <p class="tk-loader-text">Loading...<p>
+    </div>
+    <div class="tk-ml-2h tk-loader-textPos--right">
+    <i class="tk-ml-2h tk-loader-spinner"></i>
+    <p class="tk-loader-text">Loading...<p>
+    </div>
+  </div>
 </div>
 `;
