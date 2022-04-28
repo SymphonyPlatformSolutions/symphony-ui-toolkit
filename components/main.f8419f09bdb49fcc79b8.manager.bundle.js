b68fa58fc0911285bf1e1ca4458de96711a79433
Adriano Grimolizzi <adriano.grimolizzi@symphony.com>
2022-04-28 11:57:14 +0200

    Updates

--- components/main.f8419f09bdb49fcc79b8.manager.bundle.js
+++ components/main.f8419f09bdb49fcc79b8.manager.bundle.js
@@ -1,1 +1,23 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{548:function(module,exports,__webpack_require__){__webpack_require__(549),__webpack_require__(977),__webpack_require__(981),__webpack_require__(979),__webpack_require__(982),__webpack_require__(978),__webpack_require__(901),__webpack_require__(980),__webpack_require__(933),module.exports=__webpack_require__(976)},616:function(module,exports){},976:function(module,exports,__webpack_require__){"use strict";__webpack_require__(34).addons.setConfig({refs:{}})}},[[548,2,3]]]);
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [1],
+  {
+    548: function (module, exports, __webpack_require__) {
+      __webpack_require__(549),
+        __webpack_require__(977),
+        __webpack_require__(981),
+        __webpack_require__(979),
+        __webpack_require__(982),
+        __webpack_require__(978),
+        __webpack_require__(901),
+        __webpack_require__(980),
+        __webpack_require__(933),
+        (module.exports = __webpack_require__(976));
+    },
+    616: function (module, exports) {},
+    976: function (module, exports, __webpack_require__) {
+      "use strict";
+      __webpack_require__(34).addons.setConfig({ refs: {} });
+    },
+  },
+  [[548, 2, 3]],
+]);
