50209495e509480e4e8b80aa38442a7abe539fd0
Symphony-cyril <cyril.ruffa@symphony.com>
2022-04-28 13:52:12 +0200

    add unit test to component LoaderBeta

---
+++ packages/components/spec/components/loader/LoaderBeta.spec.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { LoaderBeta } from '../../../src/components/loader';
+import { render } from '@testing-library/react';
+
+describe('Loader Component', () => {
+  describe('Loader component test suite => ', () => {
+    it('should render the component with default props', () => {
+      render(<LoaderBeta />);
+
+      const targetFirstIcon = document.evaluate(
+        '//i',
+        document,
+        null,
+        XPathResult.FIRST_ORDERED_NODE_TYPE,
+        null
+      );
+      const nodeValue = targetFirstIcon.singleNodeValue;
+      expect(nodeValue).toContainHTML('tk-loader-spinner');
+    });
+
+    it('should render extra props to the loader component', () => {
+      render(
+        <LoaderBeta
+          variant="ok"
+          loadingText="loading..."
+          loadingTextPos="right"
+          size="medium"
+        >
+          Close me
+        </LoaderBeta>
+      );
+
+      const firstIcon = document.evaluate(
+        '//i',
+        document,
+        null,
+        XPathResult.FIRST_ORDERED_NODE_TYPE,
+        null
+      );
+      const nodeValue = firstIcon.singleNodeValue;
+      const firstIconText = document.evaluate(
+        '//div',
+        document,
+        null,
+        XPathResult.FIRST_ORDERED_NODE_TYPE,
+        null
+      );
+      const nodeValueText = firstIconText.singleNodeValue;
+
+      expect(nodeValue).toContainHTML('tk-loader-spinner tk-loader--ok');
+      expect(nodeValueText).toContainHTML('tk-loader-textPos--right');
+      expect(nodeValueText).toContainHTML(
+        '<p class="tk-loader-text">loading...</p>'
+      );
+      expect(nodeValue).toContainHTML('tk-loader--medium');
+    });
+  });
+});
