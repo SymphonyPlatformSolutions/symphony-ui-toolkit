a1b76e1c8b09ebc6761049dddfbe34322b73241c
Axel Eriksson <axel.eriksson@symphony.com>
2023-04-06 09:38:18 +0200

    Update icon styling in story

--- packages/components/stories/Icons.stories.tsx
+++ packages/components/stories/Icons.stories.tsx
@@ -1,12 +1,20 @@
 import * as React from 'react';
 import { SvgIcon } from '../src/components/icon';
 import { ReactIcons } from './helpers/ListOfIcons'
+import { styled, useTheme } from '@storybook/theming';
 
 export const Icons: React.FC = () => {
+  const theme: any = useTheme();
+  
   return <div>
     { Object.entries(ReactIcons).map(([key, ReactIcon]) => {
+      const StyledReactIcon = styled(ReactIcon)`
+        fill: ${theme.color.defaultText};
+        height: 24px;
+        width: 24px;
+      `
       return (<div className="preview" key={ key }>
-        <ReactIcon fill="white" height={ 24 } width={ 24 } />
+        <StyledReactIcon/>
         <br/>
         <span>{ key }</span>
       </div>
