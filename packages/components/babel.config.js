ef28238e60723254424e388074700e933a19257b
Axel Eriksson <axel.eriksson@symphony.com>
2023-10-18 22:57:35 +0200

    Update Jest and make it work with config

--- packages/components/babel.config.js
+++ packages/components/babel.config.js
@@ -1,9 +1,11 @@
 /**
- * babel.config.js is needed for
- * running jest and does not affect
- * the outputted code.
+ * This file is used by both
+ * Jest and Storybook.
  */
 module.exports = {
+  plugins: [
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+  ],
   presets: [
     '@babel/preset-env',
     '@babel/preset-react',
