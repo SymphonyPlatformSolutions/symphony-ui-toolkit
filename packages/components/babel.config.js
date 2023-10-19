82ad0da8cdf66dedd1be6935f4c26b2e16c59148
Axel Eriksson <axel.eriksson@symphony.com>
2023-10-19 11:32:45 +0200

    Put tsconfig back

--- packages/components/babel.config.js
+++ packages/components/babel.config.js
@@ -5,6 +5,8 @@
 module.exports = {
   plugins: [
     ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
   ],
   presets: [
     '@babel/preset-env',
