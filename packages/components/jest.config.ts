569b4a49b794e3b1f2d637e26b16cb4aa4022252
Axel Eriksson <axel.eriksson@symphony.com>
2023-10-24 10:26:56 +0200

    Have testrunner run esm

--- packages/components/jest.config.ts
+++ packages/components/jest.config.ts
@@ -1,18 +1,13 @@
-// For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
-module.exports = {
-  verbose: true,
+import type {Config} from 'jest';
+
+const config: Config = {
   // Automatically clear mock calls and instances between every test
   clearMocks: true,
-
   // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
-
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
-
   coveragePathIgnorePatterns: ['<rootDir>/src/components/icons/.*'],
-
   coverageThreshold: {
     global: {
       // temporarily lowering branch coverage
@@ -21,15 +16,15 @@ module.exports = {
       lines: 80,
       statements: 80,
     },
-  },
-
+  },  
   // A list of paths to modules that run some code to configure or set up
   // the testing framework before each test
   setupFilesAfterEnv: ['jest-extended', '<rootDir>spec/init/setupTests.js'],
-
   testEnvironment: 'jsdom',
-
   transformIgnorePatterns: [
     'node_modules/(?!@symphony-ui/uitoolkit-styles/dist/fonts/tk-icons.ts)',
   ],
-};
+  verbose: true,
+}
+
+export default config;
