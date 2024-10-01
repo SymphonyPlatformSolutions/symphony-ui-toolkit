83eb925ea6da9adb6fcca161058e05c8574b4fad
Pierre Hilt <pierre.hilt@symphony.com>
2024-10-01 12:33:15 +0200

    BYC-1470 make the style work with buttons

--- packages/styles/stories/navigation.stories.js
+++ packages/styles/stories/navigation.stories.js
@@ -7,13 +7,12 @@ export const Navigation = () =>
 <div class="tk-ml-2">
   <h2>Navs</h2>
   <h3>Default nav</h3>
-  <nav>
-    <ul class="tk-nav--tabs">
-      <li class="tk-nav-item">Item one</li>
-      <li class="tk-nav-item">Item two</li>
-      <li class="tk-nav-item">Item three</li>
-      <li class="tk-nav-item tk-nav-item--active">Active</li>
-    </ul>
-  </nav>
+  <div class="tk-nav--tabs">
+    <button class="tk-nav-item">Item one</button>
+    <button class="tk-nav-item">Item two</button>
+    <div class="tk-nav-item">Item three</div>
+    <button class="tk-nav-item tk-nav-item--active">Active</button>
+    <div class="tk-nav-item tk-nav-item--active">Active</div>
+  </div>
 </div>
 `;
