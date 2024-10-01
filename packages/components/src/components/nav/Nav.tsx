e43a9cde3d9e59b9e42206f9f45bb5ac64db9c78
Pierre Hilt <66311628+philt-symphony@users.noreply.github.com>
2024-10-01 18:55:20 +0200

    BYC-1470 make nav component accessible (#650)
    * BYC-1470 make the style work with buttons
    
    * BYC-1470 make nav component accessible
    
    * Add UT

--- packages/components/src/components/nav/Nav.tsx
+++ packages/components/src/components/nav/Nav.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { clsx } from 'clsx';
+import { Keys } from '../common/eventUtils';
 
 export type NavProps = {
   /** List of items displayed in the navigation bar */
@@ -13,6 +14,7 @@ export type NavProps = {
 export interface NavItem {
   label: string;
   id: number | string;
+  panelId?: number | string;
 
   // Not supported so far keep until supported
   // isDisabled?: boolean;
@@ -23,6 +25,8 @@ export const Nav: React.FC<NavProps> = ({
   activeItemId,
   onActiveTabChange,
 }: NavProps) => {
+
+  const tabs = [];
 
   const [activeNavItemId, setActiveNavItemId] = React.useState(items?.[0]?.id);
 
@@ -39,20 +43,39 @@ export const Nav: React.FC<NavProps> = ({
     }
   }
 
+  const handleKeydown = (key: string) => {
+    const localDocument = tabs[0]?.ownerDocument;
+    const focusedElementIndex = tabs.findIndex(el => el === localDocument?.activeElement);
+
+    if (!localDocument || focusedElementIndex < 0) return;
+
+    switch (key) {
+    case Keys.ARROW_RIGHT:
+      tabs[(focusedElementIndex + 1) % (tabs.length)].focus();
+      break;
+    case Keys.ARROW_LEFT:
+      focusedElementIndex === 0 ? tabs[tabs.length - 1].focus() : tabs[focusedElementIndex - 1].focus();
+      break;
+    }
+  }
+
   return (
-    <div>
-      <nav className="tk-nav">
-        <ul className="tk-nav--tabs">
-          {items.map((item) => (
-            <li
-              key={item.id}
-              onClick={() => handleClick(item)}
-              className={clsx('tk-nav-item', { 'tk-nav-item--active': activeNavItemId === item.id })}>
-              {item.label}
-            </li>
-          ))}
-        </ul>
-      </nav>
+    <div role="tablist" className="tk-nav--tabs">
+      {items.map((item, idx) => (
+        <button
+          role = "tab"
+          ref={ref => {tabs[idx] = ref}}
+          key={item.id}
+          id={'tk-tab-' + item.id}
+          aria-controls={item.panelId && 'tk-panel-' + item.panelId}
+          aria-selected={activeNavItemId === item.id}
+          onClick={() => handleClick(item)}
+          onKeyDown={(evt) => handleKeydown(evt.key)}
+          tabIndex={activeNavItemId === item.id ? 0 : -1}
+          className={clsx('tk-nav-item', { 'tk-nav-item--active': activeNavItemId === item.id })}>
+          {item.label}
+        </button>
+      ))}
     </div>
   )
 };
