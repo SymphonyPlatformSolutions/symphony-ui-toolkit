538ecf3d7cdbc2ed1d5f579cf3b9b830952861d7
Benoit Charbonnier <benoit.charbonnier@symphony.com>
2025-01-17 18:15:04 +0100

    Adding new branding icons
    Analytics
    Directory
    Messaging
    Voice

---
+++ packages/components/src/components/icons/SymphonyMessaging.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { SVGProps } from 'react';
+const SvgSymphonyMessaging = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    data-testid="ICON_SYMPHONY-MESSAGING"
+    width="1em"
+    height="1em"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M5 14h6.484L15 8l-3.516-6H5m2 6H0m9-3H2m7 6H2" stroke="currentColor" strokeWidth="1.6"/>
+  </svg>
+);
+export default SvgSymphonyMessaging;
