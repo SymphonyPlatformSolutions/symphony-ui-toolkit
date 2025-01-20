cef6894fa7d6c7fb6155d7d0544ec6963135cff3
Benoit Charbonnier <73846775+benoit-sy@users.noreply.github.com>
2025-01-20 09:21:12 +0100

    Adding new branding icons (#673)
    Analytics
    Directory
    Messaging
    Voice

---
+++ packages/components/src/components/icons/SymphonyDirectory.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { SVGProps } from 'react';
+const SvgSymphonyDirectory = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    data-testid="ICON_SYMPHONY-DIRECTORY"
+    width="1em"
+    height="1em"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M14 14V7c0-3.301-2.686-6-6-6S2 3.699 2 7v2" stroke="currentColor" strokeWidth="1.6"/>
+    <path d="M3.42 12.655A4.78 4.78 0 0 0 5 9.103V7.505a3 3 0 1 1 6-.005v2.64a10 10 0 0 1-1.168 4.695l-.229.432" stroke="currentColor" strokeWidth="1.6"/>
+    <path d="M8.8 8a.8.8 0 1 0-1.6 0zM8 10.068h-.8zM7.2 8v2.068h1.6V8zm0 2.068a6.1 6.1 0 0 1-2.021 4.538l1.07 1.189A7.7 7.7 0 0 0 8.8 10.068z" fill="currentColor"/>
+  </svg>
+);
+export default SvgSymphonyDirectory;
---
+++ packages/components/src/components/icons/SymphonyVoice.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { SVGProps } from 'react';
+const SvgSymphonyVoice = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    data-testid="ICON_SYMPHONY-VOICE"
+    width="1em"
+    height="1em"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M8.005 15.425c-4.101-4.101-4.101-10.749 0-14.849" stroke="currentColor" strokeWidth="1.6"/>
+    <path d="M10.267 13.162a7.3 7.3 0 0 1 0-10.324" stroke="currentColor" strokeWidth="1.6"/>
+  </svg>
+);
+export default SvgSymphonyVoice;
