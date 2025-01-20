e16de3e7c41f271029fa92a834c85d0c07d49964
Benoit Charbonnier <benoit.charbonnier@symphony.com>
2025-01-20 11:14:14 +0100

    fix: branding icon svgs

--- packages/components/src/components/icons/SymphonyVoice.tsx
+++ packages/components/src/components/icons/SymphonyVoice.tsx
@@ -10,8 +10,8 @@ const SvgSymphonyVoice = (props: SVGProps<SVGSVGElement>) => (
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <path d="M8.005 15.425c-4.101-4.101-4.101-10.749 0-14.849" stroke="currentColor" strokeWidth="1.6"/>
-    <path d="M10.267 13.162a7.3 7.3 0 0 1 0-10.324" stroke="currentColor" strokeWidth="1.6"/>
+    <path d="M 7.298 8.024 C 7.298 5.776 8.212 3.743 9.687 2.271 L 10.826 3.412 C 9.642 4.593 8.91 6.226 8.91 8.029 C 8.91 9.833 9.64 11.463 10.822 12.643 L 9.686 13.777 C 8.21 12.306 7.298 10.272 7.298 8.024 Z" fill="currentColor" />
+    <path d="M 4.139 8.32 C 4.042 5.079 5.315 2.114 7.432 -0.017 L 8.578 1.15 C 6.762 2.977 5.671 5.516 5.755 8.295 C 5.831 10.86 6.896 13.161 8.572 14.849 L 7.438 15.978 C 5.476 14.008 4.23 11.318 4.139 8.32 Z" fill="currentColor" />
   </svg>
 );
 export default SvgSymphonyVoice;
